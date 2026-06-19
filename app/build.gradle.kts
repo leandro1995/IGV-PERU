4dde5509793348914911cbb56c9dcc1311bbc08e
leandro1995 <leccbo1995@gmail.com>
2026-06-19 01:35:34 -0500

    :heavy_plus_sign: build.gradle.kts: Agregar la dependencia de Material Components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
