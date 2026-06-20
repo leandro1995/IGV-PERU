a5715f1a74941bfd53bbe902ce19a6eb40607db1
leandro1995 <leccbo1995@gmail.com>
2026-06-20 03:05:55 -0500

    :sparkles: ViewGallery.kt: Implementar la herencia de ViewAmbient
    Descripción:
    * Extender la clase ViewGallery para heredar de la clase base ViewAmbient
    * Sobrescribir el método Composable CompactVertical para definir la estructura de la interfaz

--- app/src/debug/java/com/pe/innari/igvperu/gallery/ViewGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/ViewGallery.kt
@@ -1,3 +1,12 @@
 package com.pe.innari.igvperu.gallery
 
-class ViewGallery
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
+
+class ViewGallery : ViewAmbient() {
+
+    @Composable
+    override fun CompactVertical() {
+
+    }
+}
