7db452f468532f60016ec5745a1bf543b8209d05
leandro1995 <leccbo1995@gmail.com>
2026-06-19 20:00:14 -0500

    :heavy_plus_sign: libs.versions.toml:  Agregar la dependencia de Kotlin Coroutines Core
    Descripción:
    * Incluir la versión 1.11.0 de coroutines Core en el catálogo de librerías
    * Configurar la implementación de la biblioteca en el archivo de construcción del módulo app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.material)
+    implementation(libs.kotlinx.coroutines.core)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
