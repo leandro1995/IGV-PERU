adfc1746d7c1398ab9b106bd1345650494c083db
leandro1995 <leccbo1995@gmail.com>
2026-06-19 20:58:24 -0500

    :sparkles: CoroutinesBackground.kt: Implementar funcionalidad de retraso en LaunchedEffect
    Descripción:
    * Agregar parámetros de tipo de tiempo y tiempo de espera al constructor de la clase
    * Configurar una suspensión con delay antes de la ejecución del método
    * Implementar lógica para convertir horas, minutos y segundos a milisegundos

--- app/src/main/java/com/pe/innari/igvperu/background/CoroutinesBackground.kt
+++ app/src/main/java/com/pe/innari/igvperu/background/CoroutinesBackground.kt
@@ -2,9 +2,32 @@ package com.pe.innari.igvperu.background
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import com.pe.innari.igvperu.background.type.TimeType
+import kotlinx.coroutines.delay
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.milliseconds
 
-class CoroutinesBackground {
+class CoroutinesBackground(private val timeType: TimeType, private val timeOut: Long = 0L) {
 
     @Composable
-    fun LaunchedEffect(method: () -> Unit) = LaunchedEffect(Unit) { method() }
+    fun LaunchedEffect(method: () -> Unit) = LaunchedEffect(Unit) {
+        if (timeOut != 0L) {
+            delay(timeType().milliseconds)
+        }
+        method()
+    }
+
+    private fun timeType() = when (timeType) {
+        TimeType.HOURS -> {
+            TimeUnit.HOURS.toMillis(timeOut)
+        }
+
+        TimeType.MINUTES -> {
+            TimeUnit.MINUTES.toMillis(timeOut)
+        }
+
+        TimeType.SECONDS -> {
+            TimeUnit.SECONDS.toMillis(timeOut)
+        }
+    }
 }
