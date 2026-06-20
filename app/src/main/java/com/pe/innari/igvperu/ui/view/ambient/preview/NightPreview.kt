b2f66f0c58e8003d1b4b3b358416318d26abadbd
leandro1995 <leccbo1995@gmail.com>
2026-06-20 03:11:34 -0500

    :sparkles: NightPreview.kt: Configurar multipreviews para diversos tamaños de pantalla y temas
    Descripción:
    * Implementar configuraciones de @Preview en las anotaciones NightPreview y NotNightPreview
    * Definir perfiles de visualización para dispositivos Phone, Medium y Expanded
    * Configurar los modos de interfaz de usuario para temas claro y oscuro
    * Establecer dimensiones específicas y fondos para cada vista previa de Compose

--- app/src/main/java/com/pe/innari/igvperu/ui/view/ambient/preview/NightPreview.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/ambient/preview/NightPreview.kt
@@ -1,3 +1,34 @@
 package com.pe.innari.igvperu.ui.view.ambient.preview
 
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "Phone Portrait",
+    widthDp = 360,
+    heightDp = 800,
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Preview(
+    name = "Phone Landscape",
+    widthDp = 800,
+    heightDp = 360,
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Preview(
+    name = "Medium",
+    widthDp = 700,
+    heightDp = 900,
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Preview(
+    name = "Expanded",
+    widthDp = 1000,
+    heightDp = 900,
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
 annotation class NightPreview
