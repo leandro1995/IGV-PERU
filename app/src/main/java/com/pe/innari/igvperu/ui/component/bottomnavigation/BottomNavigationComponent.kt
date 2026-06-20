90ff46057afeab632bc8e6a4db03d8fecc8280c4
leandro1995 <leccbo1995@gmail.com>
2026-06-20 13:54:38 -0500

    :sparkles: BottomNavigationComponent.kt: Implementar la estructura base del componente
    Descripción:
    * Extender la clase de ComponentAmbient para la gestión del ciclo de vida
    * Sobrescribir el método OnCreateView para permitir la integración con Jetpack Compose

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
@@ -1,3 +1,12 @@
 package com.pe.innari.igvperu.ui.component.bottomnavigation
 
-class BottomNavigationComponent
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+
+class BottomNavigationComponent : ComponentAmbient() {
+
+    @Composable
+    override fun OnCreateView(view: @Composable (() -> Unit)) {
+        super.OnCreateView(view)
+    }
+}
