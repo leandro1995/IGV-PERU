0bf2e31e29fcdb2dd55c47942ac46248144743fa
leandro1995 <leccbo1995@gmail.com>
2026-06-20 02:48:30 -0500

    :sparkles: Adaptable.kt: Implementar lógica de adaptabilidad para diferentes tamaños de ventana
    Descripción:
    * Agregar funciones Composable para detectar anchos compactos, medios y expandidos
    * Implementar la detección de altura compacta utilizando WindowSizeClass
    * Integrar currentWindowAdaptiveInfo para el soporte de pantallas grandes y extra grandes

--- app/src/main/java/com/pe/innari/igvperu/ui/adaptable/Adaptable.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/adaptable/Adaptable.kt
@@ -1,3 +1,30 @@
 package com.pe.innari.igvperu.ui.adaptable
 
-class Adaptable
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.Composable
+import androidx.window.core.layout.WindowSizeClass
+
+class Adaptable {
+
+    @Composable
+    private fun windowSizeClass() =
+        currentWindowAdaptiveInfo(supportLargeAndXLargeWidth = true).windowSizeClass
+
+    @Composable
+    private fun isCompactWidth() =
+        !windowSizeClass().isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_MEDIUM_LOWER_BOUND)
+
+    @Composable
+    private fun isMediumWidth() =
+        windowSizeClass().isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_MEDIUM_LOWER_BOUND) && !windowSizeClass().isWidthAtLeastBreakpoint(
+            WindowSizeClass.WIDTH_DP_EXPANDED_LOWER_BOUND
+        )
+
+    @Composable
+    private fun isExpandedWidth() =
+        windowSizeClass().isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_EXPANDED_LOWER_BOUND)
+
+    @Composable
+    private fun isCompactHeight() =
+        !windowSizeClass().isHeightAtLeastBreakpoint(WindowSizeClass.HEIGHT_DP_MEDIUM_LOWER_BOUND)
+}
