071ee9ede59a2e64c65c3eef4fce5989f5380837
leandro1995 <leccbo1995@gmail.com>
2026-06-21 05:22:36 -0500

    :sparkles: BottomNavigationGallery.kt: Implementar estructura base heredando de ViewAmbient

--- app/src/debug/java/com/pe/innari/igvperu/gallery/BottomNavigationGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/BottomNavigationGallery.kt
@@ -1,3 +1,26 @@
 package com.pe.innari.igvperu.gallery
 
-class BottomNavigationGallery
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
+import com.pe.innari.igvperu.ui.view.ambient.preview.NightPreview
+import com.pe.innari.igvperu.ui.view.ambient.preview.NotNightPreview
+
+class BottomNavigationGallery : ViewAmbient() {
+
+    @Composable
+    override fun CompactPortrait() {
+
+    }
+
+    @NightPreview
+    @Composable
+    override fun NightPreview() {
+        super.NightPreview()
+    }
+
+    @NotNightPreview
+    @Composable
+    override fun NotNightPreview() {
+        super.NotNightPreview()
+    }
+}
