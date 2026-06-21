75f08c1439e5a7de5d72de6674978a3ccd3a2940
leandro1995 <leccbo1995@gmail.com>
2026-06-21 03:45:33 -0500

    :sparkles: BottomNavigationComponent.kt: Implementar lógica de navegación adaptable
    Descripción:
    * Agregar método para determinar el tipo de navegación según el tamaño de la ventana
    * Configurar el uso de barra inferior para vistas compactas en vertical
    * Configurar el uso de riel de navegación para pantallas medianas, expandidas o compactas en horizontal

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
@@ -1,12 +1,25 @@
 package com.pe.innari.igvperu.ui.component.bottomnavigation
 
 import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.adaptable.type.ViewType
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+import com.pe.innari.igvperu.ui.component.bottomnavigation.type.BottomNavigationType
 
 class BottomNavigationComponent : ComponentAmbient() {
 
     @Composable
     override fun OnCreateView(view: @Composable (() -> Unit)) {
         super.OnCreateView(view)
+    }
+
+    @Composable
+    private fun bottomNavigationType() = when (adaptable.resolveViewTypeFromWindowSize()) {
+        ViewType.COMPACT_PORTRAIT -> {
+            BottomNavigationType.BOTTOM_NAVIGATION_BAR
+        }
+
+        ViewType.COMPACT_LAND_SCAPE, ViewType.MEDIUM, ViewType.EXPANDED -> {
+            BottomNavigationType.BOTTOM_NAVIGATION_RAIL
+        }
     }
 }
