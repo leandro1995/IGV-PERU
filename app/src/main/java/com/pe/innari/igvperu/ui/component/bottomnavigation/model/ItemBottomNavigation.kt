e2c89afb284d9d1f7b0ebf9aee933b1c126ddc44
leandro1995 <leccbo1995@gmail.com>
2026-06-21 11:43:02 -0500

    :sparkles: ItemBottomNavigation.kt: Definir propiedades de icono y etiqueta en el modelo

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/model/ItemBottomNavigation.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/model/ItemBottomNavigation.kt
@@ -1,3 +1,5 @@
 package com.pe.innari.igvperu.ui.component.bottomnavigation.model
 
-class ItemBottomNavigation
+import androidx.annotation.DrawableRes
+
+class ItemBottomNavigation(@param:DrawableRes private val icon: Int, private val label: String)
