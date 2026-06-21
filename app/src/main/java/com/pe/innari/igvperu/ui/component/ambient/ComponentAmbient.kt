b3f91a64398148158bb4a31c8e711f69559637b8
leandro1995 <leccbo1995@gmail.com>
2026-06-21 03:45:16 -0500

    :sparkles: ComponentAmbient.kt: Agregar propiedad adaptable a la clase base

--- app/src/main/java/com/pe/innari/igvperu/ui/component/ambient/ComponentAmbient.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/ambient/ComponentAmbient.kt
@@ -1,8 +1,11 @@
 package com.pe.innari.igvperu.ui.component.ambient
 
 import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.adaptable.Adaptable
 
 abstract class ComponentAmbient {
+
+    protected val adaptable = Adaptable()
 
     @Composable
     open fun OnCreateView(view: @Composable () -> Unit) {
