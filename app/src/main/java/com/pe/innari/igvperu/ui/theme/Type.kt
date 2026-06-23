853fc8bc590ee498f637f7caee6769547a4b54f6
leandro1995 <leccbo1995@gmail.com>
2026-06-23 13:12:20 -0500

    :lipstick: Type.kt: Agregar estilo de texto para el componente ItemBottomNavigation

--- app/src/main/java/com/pe/innari/igvperu/ui/theme/Type.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.pe.innari.igvperu.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pe.innari.igvperu.R
 
 val Typography = Typography(
     bodyLarge = TextStyle(
@@ -15,3 +17,6 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
 )
+
+val ItemBottomNavigationComponent =
+    TextStyle(fontSize = Text12, fontFamily = FontFamily(Font(R.font.regular)))
