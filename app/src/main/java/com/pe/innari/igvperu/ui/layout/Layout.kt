6dea9edacc8849d32b4a457cd0a904fd670a8208
leandro1995 <leccbo1995@gmail.com>
2026-06-24 01:10:14 -0500

    :sparkles: Layout.kt: Implementar lógica para el manejo de insets de pantalla
    Descripción:
    * Agregar funciones para calcular el padding horizontal de forma dinámica
    * Combinar insets de recorte de pantalla (displayCutout) y bordes curvados (waterfall)
    * Utilizar LocalLayoutDirection para asegurar la compatibilidad con diferentes direcciones de diseño

--- app/src/main/java/com/pe/innari/igvperu/ui/layout/Layout.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/layout/Layout.kt
@@ -1,3 +1,28 @@
 package com.pe.innari.igvperu.ui.layout
 
-class Layout
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.layout.waterfall
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalLayoutDirection
+
+class Layout {
+
+    @Composable
+    fun calculateStartPadding() =
+        paddingValue().calculateStartPadding(localLayoutDirectionCurrent())
+
+    @Composable
+    fun calculateEndPadding() = paddingValue().calculateEndPadding(localLayoutDirectionCurrent())
+
+    @Composable
+    private fun localLayoutDirectionCurrent() = LocalLayoutDirection.current
+
+    @Composable
+    private fun paddingValue() =
+        WindowInsets.displayCutout.union(WindowInsets.waterfall).asPaddingValues()
+}
