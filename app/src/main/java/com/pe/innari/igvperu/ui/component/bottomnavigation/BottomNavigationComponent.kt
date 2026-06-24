b20ca9ed725ee1c1d096a1cbec7e2deb45190159
leandro1995 <leccbo1995@gmail.com>
2026-06-24 02:20:08 -0500

    :sparkles: BottomNavigationComponent.kt: Implementar soporte para NavigationRail y mejorar la adaptabilidad UI
    Descripción:
    * Implementar el componente NavigationRail para mejorar la experiencia en pantallas grandes
    * Refactorizar CardContent para permitir la personalización mediante modificadores
    * Configurar el manejo dinámico de insets y paddings según el tipo de dispositivo
    * Definir estilos y colores específicos para los elementos de la navegación lateral

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
@@ -1,25 +1,37 @@
 package com.pe.innari.igvperu.ui.component.bottomnavigation
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.NavigationRailItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -29,6 +41,7 @@ import com.pe.innari.igvperu.ui.adaptable.type.ViewType
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
 import com.pe.innari.igvperu.ui.component.bottomnavigation.model.ItemBottomNavigation
 import com.pe.innari.igvperu.ui.component.bottomnavigation.type.BottomNavigationType
+import com.pe.innari.igvperu.ui.theme.Dimen0
 import com.pe.innari.igvperu.ui.theme.Dimen1
 import com.pe.innari.igvperu.ui.theme.Dimen16
 import com.pe.innari.igvperu.ui.theme.Dimen22
@@ -46,7 +59,7 @@ class BottomNavigationComponent(private val itemBottomNavigationMutableList: Mut
 
         indexSelect = rememberSaveable { mutableIntStateOf(0) }
     }
-    
+
     @Composable
     override fun OnCreateView(view: @Composable (() -> Unit)) {
         super.OnCreateView(view)
@@ -74,18 +87,37 @@ class BottomNavigationComponent(private val itemBottomNavigationMutableList: Mut
 
     @Composable
     private fun BottomNavigationRail(view: @Composable () -> Unit) {
-        Row(modifier = Modifier.fillMaxSize()) {
-            Box(
-                modifier = Modifier
-                    .weight(1F)
-                    .fillMaxSize()
-            ) { view() }
+        Row(
+            modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)
+        ) {
+            CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onBackground) {
+                Row(modifier = Modifier.windowInsetsPadding(insets = insets())) {
+                    Box(
+                        modifier = Modifier.fillMaxHeight().padding(
+                            start = maxOf(horizontalInset(), startPaddingNavigationRail()),
+                            end = Dimen16
+                        )
+                    ) {
+                        NavigationRail()
+                    }
+                    Box(
+                        modifier = Modifier.padding(
+                            top = topPaddingNavigationRail(), bottom = bottomPaddingNavigationRail()
+                        )
+                    ) {
+                        view()
+                    }
+                }
+            }
         }
     }
 
     @Composable
     private fun NavigationBar() {
-        CardContent {
+        CardContent(
+            modifier = Modifier.fillMaxWidth().navigationBarsPadding()
+                .padding(start = Dimen16, end = Dimen16)
+        ) {
             NavigationBar(containerColor = MaterialTheme.colorScheme.surface) {
                 itemBottomNavigationMutableList.forEachIndexed { index, item ->
                     NavigationBarItem(
@@ -110,11 +142,40 @@ class BottomNavigationComponent(private val itemBottomNavigationMutableList: Mut
     }
 
     @Composable
-    private fun CardContent(view: @Composable () -> Unit) = Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .navigationBarsPadding()
-            .padding(start = Dimen16, end = Dimen16),
+    private fun NavigationRail() {
+        CardContent(
+            modifier = Modifier.padding(
+                top = topPaddingNavigationRail(), bottom = bottomPaddingNavigationRail()
+            )
+        ) {
+            NavigationRail(
+                modifier = Modifier.padding(top = Dimen16, bottom = Dimen16),
+                containerColor = MaterialTheme.colorScheme.surface,
+                windowInsets = WindowInsets(Dimen0)
+            ) {
+                itemBottomNavigationMutableList.forEachIndexed { index, item ->
+                    NavigationRailItem(
+                        colors = navigationRailItemDefaultsColors(),
+                        selected = index == indexSelect.intValue,
+                        onClick = { indexSelect.intValue = index },
+                        icon = {
+                            Icon(
+                                modifier = Modifier.size(Dimen24),
+                                painter = painterResource(item.icon),
+                                contentDescription = null
+                            )
+                        },
+                        label = {
+                            Text(text = item.label, style = ItemBottomNavigationComponent)
+                        })
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun CardContent(modifier: Modifier = Modifier, view: @Composable () -> Unit) = Card(
+        modifier = modifier,
         shape = RoundedCornerShape(Dimen22),
         border = BorderStroke(width = Dimen1, color = MaterialTheme.colorScheme.outline),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
@@ -130,6 +191,45 @@ class BottomNavigationComponent(private val itemBottomNavigationMutableList: Mut
         unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
         unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant
     )
+
+    @Composable
+    private fun navigationRailItemDefaultsColors() = NavigationRailItemDefaults.colors(
+        indicatorColor = MaterialTheme.colorScheme.primaryContainer,
+        selectedIconColor = MaterialTheme.colorScheme.primary,
+        selectedTextColor = MaterialTheme.colorScheme.primary,
+        unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant
+    )
+
+    @Composable
+    private fun insets() = WindowInsets.safeDrawing.only(
+        sides = WindowInsetsSides.Top + WindowInsetsSides.Bottom
+    )
+
+    @Composable
+    private fun horizontalInset() =
+        maxOf(layout.calculateStartPadding(), layout.calculateEndPadding())
+
+    @Composable
+    private fun startPaddingNavigationRail() = if (adaptable.isPhoneOrTablet()) {
+        Dimen0
+    } else {
+        Dimen24
+    }
+
+    @Composable
+    private fun topPaddingNavigationRail() = if (adaptable.isPhoneOrTablet()) {
+        Dimen0
+    } else {
+        Dimen16
+    }
+
+    @Composable
+    private fun bottomPaddingNavigationRail() = if (adaptable.isPhoneOrTablet()) {
+        Dimen0
+    } else {
+        Dimen16
+    }
 
     @Composable
     private fun bottomNavigationType() = when (adaptable.resolveViewTypeFromWindowSize()) {
