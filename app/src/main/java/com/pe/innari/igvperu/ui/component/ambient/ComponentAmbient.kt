c2bbfa98c0f277ddb35f6f4bd6f4044d3e35b4b3
leandro1995 <leccbo1995@gmail.com>
2026-06-24 01:14:55 -0500

    :sparkles: ComponentAmbient.kt: Agregar la propiedad layout a la clase base

--- app/src/main/java/com/pe/innari/igvperu/ui/component/ambient/ComponentAmbient.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/ambient/ComponentAmbient.kt
@@ -2,10 +2,12 @@ package com.pe.innari.igvperu.ui.component.ambient
 
 import androidx.compose.runtime.Composable
 import com.pe.innari.igvperu.ui.adaptable.Adaptable
+import com.pe.innari.igvperu.ui.layout.Layout
 
 abstract class ComponentAmbient {
 
     protected val adaptable = Adaptable()
+    protected val layout = Layout()
 
     @Composable
     protected open fun Instance() {
