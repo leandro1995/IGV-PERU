1df1d86bc4876a8529892bdb8e7839209aaa70d9
leandro1995 <leccbo1995@gmail.com>
2026-06-24 00:43:45 -0500

    :sparkles: Dimen.kt: Agregar la constante de dimensión Dimen0

--- app/src/main/java/com/pe/innari/igvperu/ui/theme/Dimen.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/theme/Dimen.kt
@@ -3,6 +3,7 @@ package com.pe.innari.igvperu.ui.theme
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+val Dimen0 = 0.dp
 val Dimen1 = 1.dp
 val Dimen16 = 16.dp
 val Dimen22 = 22.dp
