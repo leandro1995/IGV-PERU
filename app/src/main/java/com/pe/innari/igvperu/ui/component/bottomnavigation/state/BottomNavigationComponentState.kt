ddc8ad31c2986a71403c26db17cf4bf4d5a50129
leandro1995 <leccbo1995@gmail.com>
2026-06-24 22:44:00 -0500

    :sparkles: BottomNavigationComponentState.kt: Crear la clase de estado para el componente de navegación inferior

---
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/state/BottomNavigationComponentState.kt
@@ -0,0 +1,7 @@
+package com.pe.innari.igvperu.ui.component.bottomnavigation.state
+
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.Stable
+
+@Stable
+class BottomNavigationComponentState(val indexSelect: MutableIntState)
