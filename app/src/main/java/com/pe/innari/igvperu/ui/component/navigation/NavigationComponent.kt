dd86f560f64676b7c6e69649a8adc2d4e3b1acd9
leandro1995 <leccbo1995@gmail.com>
2026-06-25 14:18:31 -0500

    :sparkles: NavigationComponent.kt: Implementar la visualización de navegación
    Descripción:
    * Agregar el parámetro backStack al constructor para gestionar el estado de navegación
    * Integrar el componente NavDisplay dentro del método OnCreateView
    * Configurar la lógica de retroceso manual para remover elementos de la pila de navegación

--- app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponent.kt
@@ -1,12 +1,22 @@
 package com.pe.innari.igvperu.ui.component.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.ui.NavDisplay
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
 
-class NavigationComponent : ComponentAmbient() {
+class NavigationComponent(private val backStack: NavBackStack<NavKey>) : ComponentAmbient() {
 
     @Composable
     override fun OnCreateView() {
         super.OnCreateView()
+
+        NavDisplay(backStack = backStack, onBack = {
+            if (backStack.isNotEmpty()) {
+                backStack.removeAt(backStack.lastIndex)
+            }
+        }, entryProvider = entryProvider { })
     }
 }
