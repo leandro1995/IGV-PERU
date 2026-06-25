d342cac93700bc95c3fe4e36a00f9b7c46d7969c
leandro1995 <leccbo1995@gmail.com>
2026-06-25 02:13:42 -0500

    :sparkles: NavigationComponent.kt: Implementar la estructura básica del componente

--- app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponent.kt
@@ -1,3 +1,12 @@
 package com.pe.innari.igvperu.ui.component.navigation
 
-class NavigationComponent
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+
+class NavigationComponent : ComponentAmbient() {
+
+    @Composable
+    override fun OnCreateView() {
+        super.OnCreateView()
+    }
+}
