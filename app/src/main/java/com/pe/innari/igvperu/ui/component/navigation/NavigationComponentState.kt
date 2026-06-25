d5e7b4c3a3890972d42cb4f36465138fd5fe7436
leandro1995 <leccbo1995@gmail.com>
2026-06-25 14:18:53 -0500

    :sparkles: NavigationComponentState.kt: Implementar el estado para el componente de navegación
    Descripción:
    * Agregar la propiedad backStack para gestionar la pila de navegación con Navigation3
    * Anotar la clase con @Stable para optimizar la estabilidad y el rendimiento en Compose

--- app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponentState.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponentState.kt
@@ -1,3 +1,10 @@
 package com.pe.innari.igvperu.ui.component.navigation
 
-class NavigationComponentState
+import androidx.compose.runtime.Stable
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+
+@Stable
+class NavigationComponentState(
+    val backStack: NavBackStack<NavKey>
+)
