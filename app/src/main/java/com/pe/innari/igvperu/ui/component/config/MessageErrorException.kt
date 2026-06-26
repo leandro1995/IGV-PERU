b8b474dac58893bfd15ba23674715630ae3acb7f
leandro1995 <leccbo1995@gmail.com>
2026-06-26 03:35:06 -0500

    :sparkles: MessageErrorException.kt: Agregar mensaje de error para el proveedor de navegación

--- app/src/main/java/com/pe/innari/igvperu/ui/component/config/MessageErrorException.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/config/MessageErrorException.kt
@@ -1,3 +1,7 @@
 package com.pe.innari.igvperu.ui.component.config
 
-object MessageErrorException
+object MessageErrorException {
+
+    const val EMPTY_PROVIDER_MESSAGE_ERROR_EXCEPTION_NAVIGATION =
+        "Primero se debe ejecutar la función `setEntryProvider` para establecer un valor predeterminado para `entryProvider`."
+}
