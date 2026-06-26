257973ebf1fddc0e1ad5a3c0d8c58e0bc18cd213
leandro1995 <leccbo1995@gmail.com>
2026-06-25 20:54:11 -0500

    :sparkles: NavigationComponent.kt: Implementar mecanismo de configuración para el proveedor de entradas
    Descripción:
    * Agregar propiedad privada para gestionar el callback de configuración del proveedor de entradas
    * Actualizar la llamada a NavDisplay para ejecutar la lógica de navegación mediante el callback delegado
    * Implementar la función pública setEntryProvider para permitir la definición dinámica de rutas desde el exterior

--- app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponent.kt
@@ -1,13 +1,17 @@
 package com.pe.innari.igvperu.ui.component.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+import com.pe.innari.igvperu.ui.component.navigation.config.callback.NavigationComponentConfigCallBack
 
 class NavigationComponent(private val backStack: NavBackStack<NavKey>) : ComponentAmbient() {
+
+    private var entryProviderScopeCallBack: NavigationComponentConfigCallBack? = null
 
     @Composable
     override fun OnCreateView() {
@@ -17,6 +21,18 @@ class NavigationComponent(private val backStack: NavBackStack<NavKey>) : Compone
             if (backStack.isNotEmpty()) {
                 backStack.removeAt(backStack.lastIndex)
             }
-        }, entryProvider = entryProvider { })
+        }, entryProvider = entryProvider {
+            entryProviderScopeCallBack?.entryProviderScope(entryProviderScope = this)
+        })
+    }
+
+    fun setEntryProvider(entryProvider: (entry: EntryProviderScope<NavKey>) -> Unit) {
+        if (entryProviderScopeCallBack == null) {
+            entryProviderScopeCallBack = object : NavigationComponentConfigCallBack {
+                override fun entryProviderScope(entryProviderScope: EntryProviderScope<NavKey>) {
+                    entryProvider(entryProviderScope)
+                }
+            }
+        }
     }
 }
