fb674de8d0e796f15ab10cb02074ae276082fba8
leandro1995 <leccbo1995@gmail.com>
2026-06-25 20:54:33 -0500

    :sparkles: NavigationComponentConfigCallBack.kt: Agregar el método entryProviderScope a la interfaz

--- app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/config/callback/NavigationComponentConfigCallBack.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/config/callback/NavigationComponentConfigCallBack.kt
@@ -1,3 +1,9 @@
 package com.pe.innari.igvperu.ui.component.navigation.config.callback
 
-interface NavigationComponentConfigCallBack
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavKey
+
+interface NavigationComponentConfigCallBack {
+
+    fun entryProviderScope(entryProviderScope: EntryProviderScope<NavKey>)
+}
