1a5232a4ba328e0beb57d46f589a97f9351889b1
leandro1995 <leccbo1995@gmail.com>
2026-06-27 13:53:11 -0500

    :sparkles: HomeViewState.kt: Agregar backStack al estado de la vista de inicio

--- app/src/main/java/com/pe/innari/igvperu/ui/view/home/state/HomeViewState.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/home/state/HomeViewState.kt
@@ -2,8 +2,10 @@ package com.pe.innari.igvperu.ui.view.home.state
 
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.Stable
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
 
 @Stable
 class HomeViewState(
-    val indexSelect: MutableIntState
+    val indexSelect: MutableIntState, val backStack: NavBackStack<NavKey>
 )
