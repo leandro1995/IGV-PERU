b8dba58720c184efc30770eb9eeb28541e3ce64b
leandro1995 <leccbo1995@gmail.com>
2026-06-26 22:08:00 -0500

    :sparkles: HomeView.kt: Implementar la estructura básica de la vista principal
    Descripción:
    * Heredar de ViewAmbient para integrar el soporte de diseños adaptativos
    * Implementar el método CompactPortrait para definir la interfaz en modo vertical
    * Configurar las funciones de previsualización para los modos claro y oscuro

--- app/src/main/java/com/pe/innari/igvperu/ui/view/home/HomeView.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/home/HomeView.kt
@@ -1,3 +1,26 @@
 package com.pe.innari.igvperu.ui.view.home
 
-class HomeView
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
+import com.pe.innari.igvperu.ui.view.ambient.preview.NightPreview
+import com.pe.innari.igvperu.ui.view.ambient.preview.NotNightPreview
+
+class HomeView : ViewAmbient() {
+
+    @Composable
+    override fun CompactPortrait() {
+
+    }
+
+    @NotNightPreview
+    @Composable
+    override fun NotNightPreview() {
+        super.NotNightPreview()
+    }
+
+    @NightPreview
+    @Composable
+    override fun NightPreview() {
+        super.NightPreview()
+    }
+}
