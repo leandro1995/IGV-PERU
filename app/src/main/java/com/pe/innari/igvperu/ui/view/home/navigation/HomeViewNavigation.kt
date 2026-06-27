a8bd7815f08472bba69bc6c53c8bacf87b454b9b
leandro1995 <leccbo1995@gmail.com>
2026-06-27 13:43:43 -0500

    :sparkles: HomeViewNavigation.kt: Definir las rutas de navegación para la vista principal
    Descripción:
    * Implementar la clase sellada HomeViewNavigation con soporte para NavKey
    * Agregar los destinos serializables Calculator, History, Calentar y Setting

--- app/src/main/java/com/pe/innari/igvperu/ui/view/home/navigation/HomeViewNavigation.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/home/navigation/HomeViewNavigation.kt
@@ -1,3 +1,18 @@
 package com.pe.innari.igvperu.ui.view.home.navigation
 
-class HomeViewNavigation
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+sealed class HomeViewNavigation {
+    @Serializable
+    object Calculator : NavKey
+
+    @Serializable
+    object History : NavKey
+
+    @Serializable
+    object Calentar : NavKey
+
+    @Serializable
+    object Setting : NavKey
+}
