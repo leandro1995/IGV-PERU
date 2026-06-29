97515d01cf3f91f217348e9972ecf5fa7ae66010
leandro1995 <leccbo1995@gmail.com>
2026-06-29 03:59:41 -0500

    :sparkles: ToolBarComponent.kt: Implementar estructura visual del componente Toolbar
    Descripción:
    * Implementar el diseño de la interfaz de usuario utilizando ElevatedCard, Row y Column
    * Integrar la visualización del icono y los textos de título y subtítulo desde el modelo de datos
    * Configurar modificadores de diseño para asegurar que el componente ocupe el ancho total disponible

--- app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/ToolBarComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/ToolBarComponent.kt
@@ -1,6 +1,15 @@
 package com.pe.innari.igvperu.ui.component.toolbar
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
 import com.pe.innari.igvperu.ui.component.toolbar.model.ToolBar
 
@@ -9,5 +18,18 @@ class ToolBarComponent(private val toolBar: ToolBar) : ComponentAmbient() {
     @Composable
     override fun OnCreateComponent() {
         super.OnCreateComponent()
+
+        ElevatedCard(modifier = Modifier.fillMaxWidth()) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Column {
+                    Icon(painter = painterResource(toolBar.icon), contentDescription = null)
+                }
+                Spacer(modifier = Modifier)
+                Column {
+                    Text(text = toolBar.title)
+                    Text(text = toolBar.subTitle)
+                }
+            }
+        }
     }
 }
