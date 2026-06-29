05264cafca49d2351229c0ebd5823c6d615a6f39
leandro1995 <leccbo1995@gmail.com>
2026-06-29 03:59:55 -0500

    :recycle: ToolBar.kt: Cambiar visibilidad de las propiedades a pública

--- app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/model/ToolBar.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/model/ToolBar.kt
@@ -3,7 +3,5 @@ package com.pe.innari.igvperu.ui.component.toolbar.model
 import androidx.annotation.DrawableRes
 
 class ToolBar(
-    @param:DrawableRes private val icon: Int,
-    private val title: String,
-    private val subTitle: String
+    @param:DrawableRes val icon: Int, val title: String, val subTitle: String
 )
