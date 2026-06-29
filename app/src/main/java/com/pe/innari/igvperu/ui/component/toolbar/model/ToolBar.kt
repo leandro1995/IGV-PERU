7867a5f555b560ae9eede3f4d985563e1262ce9a
leandro1995 <leccbo1995@gmail.com>
2026-06-29 03:50:35 -0500

    :sparkles: ToolBar.kt: Agregar propiedades de icono, título y subtítulo al modelo

--- app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/model/ToolBar.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/model/ToolBar.kt
@@ -1,3 +1,9 @@
 package com.pe.innari.igvperu.ui.component.toolbar.model
 
-class ToolBar
+import androidx.annotation.DrawableRes
+
+class ToolBar(
+    @param:DrawableRes private val icon: Int,
+    private val title: String,
+    private val subTitle: String
+)
