116408376240309269984d84433d82fb24622618
leandro1995 <leccbo1995@gmail.com>
2026-06-29 02:35:27 -0500

    :sparkles: ToolBarComponent.kt: Implementar herencia de ComponentAmbient y método OnCreateComponent

--- app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/ToolBarComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/ToolBarComponent.kt
@@ -1,3 +1,12 @@
 package com.pe.innari.igvperu.ui.component.toolbar
 
-class ToolBarComponent
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+
+class ToolBarComponent : ComponentAmbient() {
+
+    @Composable
+    override fun OnCreateComponent() {
+        super.OnCreateComponent()
+    }
+}
