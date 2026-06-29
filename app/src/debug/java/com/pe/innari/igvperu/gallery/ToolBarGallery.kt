24b441d9c57ba0ed3fac47cddf8f1503905a5c47
leandro1995 <leccbo1995@gmail.com>
2026-06-29 02:30:38 -0500

    :sparkles: ToolBarGallery.kt: Implementar estructura base y métodos de previsualización
    Descripción:
    * Extender la clase ViewAmbient para la gestión de layouts adaptables
    * Implementar el método CompactPortrait para definir la interfaz en modo vertical
    * Agregar funciones de previsualización para los modos claro y oscuro mediante NightPreview y NotNightPreview

--- app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarGallery.kt
@@ -1,3 +1,26 @@
 package com.pe.innari.igvperu.gallery
 
-class ToolBarGallery
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
+import com.pe.innari.igvperu.ui.view.ambient.preview.NightPreview
+import com.pe.innari.igvperu.ui.view.ambient.preview.NotNightPreview
+
+class ToolBarGallery : ViewAmbient() {
+
+    @Composable
+    override fun CompactPortrait() {
+
+    }
+
+    @NotNightPreview
+    @Composable
+    override fun NotNightPreview() {
+        super.NotNightPreview()
+    }
+
+    @NightPreview
+    @Composable
+    override fun NightPreview() {
+        super.NightPreview()
+    }
+}
