f9da4a0fe8b49080e02bd28f1815eda7f5668c19
leandro1995 <leccbo1995@gmail.com>
2026-06-29 20:22:54 -0500

    :sparkles: ToolBarComponentElevation.kt: Implementar el método cardDefaultsCardElevation para definir la elevación

--- app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/elevation/ToolBarComponentElevation.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/elevation/ToolBarComponentElevation.kt
@@ -1,3 +1,14 @@
 package com.pe.innari.igvperu.ui.component.toolbar.elevation
 
-class ToolBarComponentElevation
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.theme.Dimen6
+
+class ToolBarComponentElevation {
+
+    companion object {
+
+        @Composable
+        fun cardDefaultsCardElevation() = CardDefaults.cardElevation(defaultElevation = Dimen6)
+    }
+}
