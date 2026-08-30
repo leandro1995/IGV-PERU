2384140807c76b08c59738a884a549ca4f162467
leandro1995 <leccbo1995@gmail.com>
2026-08-30 01:32:34 -0500

    :memo: ViewAmbient.kt: Agregar documentación a la clase base y sus métodos
    Descripción:
    * Documentar el propósito de la clase abstracta ViewAmbient
    * Describir la función OnCreate para la creación de la interfaz de usuario
    * Explicar la implementación de la función Preview con el tema IGVPERUTheme

--- app/src/main/java/com/pe/innari/igvperu/ui/view/ambient/ViewAmbient.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/ambient/ViewAmbient.kt
@@ -3,11 +3,23 @@ package com.pe.innari.igvperu.ui.view.ambient
 import androidx.compose.runtime.Composable
 import com.pe.innari.igvperu.ui.theme.IGVPERUTheme
 
+/**
+ * Clase abstracta base para definir vistas que requieren una estructura común y la aplicación de un tema.
+ *
+ * Proporciona una forma estandarizada de implementar la interfaz de usuario y su previsualización correspondiente.
+ */
 abstract class ViewAmbient {
 
+    /**
+     * Función abstracta que debe ser implementada por las subclases para definir el contenido de la interfaz de usuario Composable.
+     */
     @Composable
     abstract fun OnCreate()
 
+    /**
+     * Implementación por defecto para previsualizar la vista.
+     * Envuelve el contenido de [OnCreate] dentro del tema [IGVPERUTheme].
+     */
     @Composable
     open fun Preview() {
         IGVPERUTheme {
