fa69dccfb1bf281ed8b9c76686cc759b7b7a84bf
leandro1995 <leccbo1995@gmail.com>
2026-08-30 00:00:00 -0500

    :sparkles: ViewGallery.kt: Implementar estructura base y previsualización del componente
    Descripción:
    * Heredar de la clase ViewAmbient para integrar el componente en el flujo de la aplicación
    * Implementar el método OnCreate para renderizar un mensaje de texto inicial
    * Configurar la anotación Preview para habilitar la vista previa en el entorno de desarrollo

--- app/src/debug/java/com/pe/innari/igvperu/gallery/ViewGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/ViewGallery.kt
@@ -1,3 +1,20 @@
 package com.pe.innari.igvperu.gallery
 
-class ViewGallery
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
+
+class ViewGallery : ViewAmbient() {
+
+    @Composable
+    override fun OnCreate() {
+        Text("Hola mundo")
+    }
+
+    @Preview
+    @Composable
+    override fun Preview() {
+        super.Preview()
+    }
+}
