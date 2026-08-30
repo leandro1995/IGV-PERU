4e6ad035996c4b51a1406bf3175c423404a3b973
leandro1995 <leccbo1995@gmail.com>
2026-08-30 00:44:35 -0500

    :sparkles: ThemeAdaptivePreview.kt: Implementar anotación para vistas previas adaptativas
    Descripción:
    * Definir configuraciones de vista previa para teléfonos y tabletas
    * Incluir soporte para orientaciones vertical y horizontal
    * Configurar variantes para modo claro y modo oscuro
    * Establecer la clase de anotación para facilitar el diseño responsivo en Compose

--- app/src/main/java/com/pe/innari/igvperu/ui/view/adaptivepreview/ThemeAdaptivePreview.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/adaptivepreview/ThemeAdaptivePreview.kt
@@ -1,3 +1,73 @@
 package com.pe.innari.igvperu.ui.view.adaptivepreview
 
-class ThemeAdaptivePreview
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "Phone Portrait - Light",
+    group = "Adaptive Light",
+    widthDp = 360,
+    heightDp = 800,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+@Preview(
+    name = "Phone Portrait - Dark",
+    group = "Adaptive Dark",
+    widthDp = 360,
+    heightDp = 800,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+
+@Preview(
+    name = "Phone Landscape - Light",
+    group = "Adaptive Light",
+    widthDp = 800,
+    heightDp = 360,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+@Preview(
+    name = "Phone Landscape - Dark",
+    group = "Adaptive Dark",
+    widthDp = 800,
+    heightDp = 360,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+
+@Preview(
+    name = "Tablet Portrait - Light",
+    group = "Adaptive Light",
+    widthDp = 800,
+    heightDp = 1280,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+@Preview(
+    name = "Tablet Portrait - Dark",
+    group = "Adaptive Dark",
+    widthDp = 800,
+    heightDp = 1280,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+
+@Preview(
+    name = "Tablet Landscape - Light",
+    group = "Adaptive Light",
+    widthDp = 1280,
+    heightDp = 800,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+@Preview(
+    name = "Tablet Landscape - Dark",
+    group = "Adaptive Dark",
+    widthDp = 1280,
+    heightDp = 800,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+annotation class ThemeAdaptivePreview
