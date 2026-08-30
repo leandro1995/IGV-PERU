0507ea8baf92285e364adb5efc04d28ad156610a
leandro1995 <leccbo1995@gmail.com>
2026-08-30 01:11:13 -0500

    :lipstick: ViewGallery.kt: Implementar Scaffold y actualizar anotación de vista previa
    Descripción:
    * Implementar el componente Scaffold para gestionar el diseño base y el relleno interno
    * Reemplazar la anotación @Preview por @ThemeAdaptivePreview para admitir vistas adaptativas
    * Aplicar modificadores de tamaño completo y espaciado al componente de texto en el método OnCreate

--- app/src/debug/java/com/pe/innari/igvperu/gallery/ViewGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/ViewGallery.kt
@@ -1,18 +1,28 @@
 package com.pe.innari.igvperu.gallery
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.pe.innari.igvperu.Greeting
+import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
 
 class ViewGallery : ViewAmbient() {
 
     @Composable
     override fun OnCreate() {
-        Text("Hola mundo")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Text(
+                text = "Hola mundo", modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 
-    @Preview
+    @ThemeAdaptivePreview
     @Composable
     override fun Preview() {
         super.Preview()
