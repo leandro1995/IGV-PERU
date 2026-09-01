c9bf064607377acf7651d79d0a24a21265479ae4
leandro1995 <leccbo1995@gmail.com>
2026-09-01 02:21:54 -0500

    :sparkles: ViewWindowSize.kt: Implementar lógica para clases de tamaño de ventana y orientación
    Descripción:
    * Calcular las dimensiones de la pantalla en dp utilizando LocalConfiguration
    * Clasificar el ancho y el alto en categorías de WindowSizeClass (Compact, Medium, Expanded)
    * Implementar la detección de la orientación actual del dispositivo
    * Definir constantes para los umbrales de tamaño de ventana compactos y medianos

--- app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/ViewWindowSize.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/ViewWindowSize.kt
@@ -1,3 +1,72 @@
 package com.pe.innari.igvperu.ui.view.windowsize
 
-class ViewWindowSize
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.res.Configuration
+import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import com.pe.innari.igvperu.ui.view.windowsize.orientation.OrientationWindowSize
+
+class ViewWindowSize(private val activity: Activity) {
+
+    @Composable
+    private fun localConfigurationCurrent() = LocalConfiguration.current
+
+    @SuppressLint("ConfigurationScreenWidthHeight")
+    @Composable
+    private fun width() = localConfigurationCurrent().screenWidthDp
+
+    @SuppressLint("ConfigurationScreenWidthHeight")
+    @Composable
+    private fun height() = localConfigurationCurrent().screenHeightDp
+
+    @Composable
+    private fun widthSize() = when {
+        width() < COMPACT_WIDTH_SIZE -> {
+            WindowWidthSizeClass.Compact
+        }
+
+        width() < MEDIUM_WIDTH_SIZE -> {
+            WindowWidthSizeClass.Medium
+        }
+
+        else -> {
+            WindowWidthSizeClass.Expanded
+        }
+    }
+
+    @Composable
+    private fun heightSize() = when {
+        height() < COMPACT_HEIGHT_SIZE -> {
+            WindowHeightSizeClass.Compact
+        }
+
+        height() < MEDIUM_HEIGHT_SIZE -> {
+            WindowHeightSizeClass.Medium
+        }
+
+        else -> {
+            WindowHeightSizeClass.Expanded
+        }
+    }
+
+    @Composable
+    private fun orientation() = when (localConfigurationCurrent().orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            OrientationWindowSize.LANDSCAPE
+        }
+
+        else -> {
+            OrientationWindowSize.PORTRAIT
+        }
+    }
+
+    companion object {
+        private const val COMPACT_WIDTH_SIZE = 600
+        private const val MEDIUM_WIDTH_SIZE = 8400
+        private const val COMPACT_HEIGHT_SIZE = 480
+        private const val MEDIUM_HEIGHT_SIZE = 900
+    }
+}
