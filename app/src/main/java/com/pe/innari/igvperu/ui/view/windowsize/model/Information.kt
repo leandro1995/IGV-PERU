c174423782b8648b097a7192ebcb44c1a8464418
leandro1995 <leccbo1995@gmail.com>
2026-09-01 02:00:41 -0500

    :sparkles: Information.kt: Implementar lógica para el manejo de tamaños de ventana y orientación
    Descripción:
    * Agregar parámetros al constructor para el ancho, alto, orientación y tipo de ventana
    * Implementar métodos para validar si el tamaño del ancho es compacto, medio o expandido
    * Definir funciones auxiliares para determinar si la orientación es vertical u horizontal

--- app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/model/Information.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/model/Information.kt
@@ -1,3 +1,24 @@
 package com.pe.innari.igvperu.ui.view.windowsize.model
 
-class Information
+import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import com.pe.innari.igvperu.ui.view.windowsize.orientation.OrientationWindowSize
+import com.pe.innari.igvperu.ui.view.windowsize.type.TypeWindowSize
+
+class Information(
+    private val windowWidthSizeClass: WindowWidthSizeClass,
+    private val windowHeightSizeClass: WindowHeightSizeClass,
+    private val orientationWindowSize: OrientationWindowSize,
+    private val typeWindowSize: TypeWindowSize
+) {
+
+    fun isCompact() = windowWidthSizeClass == WindowWidthSizeClass.Compact
+
+    fun isMedium() = windowWidthSizeClass == WindowWidthSizeClass.Medium
+
+    fun isExpanded() = windowWidthSizeClass == WindowWidthSizeClass.Expanded
+
+    fun isPortrait() = orientationWindowSize == OrientationWindowSize.PORTRAIT
+
+    fun isLandscape() = orientationWindowSize == OrientationWindowSize.LANDSCAPE
+}
