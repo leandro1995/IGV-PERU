c78b924475ff8f87d08e1fbb8ff793643c720943
leandro1995 <leccbo1995@gmail.com>
2026-09-01 01:42:51 -0500

    :sparkles: TypeWindowSize.kt: Definir los tipos de tamaño de ventana para dispositivos

--- app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/type/TypeWindowSize.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/type/TypeWindowSize.kt
@@ -1,3 +1,5 @@
 package com.pe.innari.igvperu.ui.view.windowsize.type
 
-enum class TypeWindowSize
+enum class TypeWindowSize {
+    PORTRAIT_PHONE, LANDSCAPE_PHONE, PORTRAIT_TABLET, LANDSCAPE_TABLET
+}
