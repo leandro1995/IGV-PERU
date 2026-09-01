087b0ce036890015bef845058199e0b5585d00c3
leandro1995 <leccbo1995@gmail.com>
2026-09-01 02:01:13 -0500

    :recycle: Information.kt: Simplificar la clase eliminando parámetros y dependencias no utilizados

--- app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/model/Information.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/model/Information.kt
@@ -1,15 +1,11 @@
 package com.pe.innari.igvperu.ui.view.windowsize.model
 
-import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import com.pe.innari.igvperu.ui.view.windowsize.orientation.OrientationWindowSize
-import com.pe.innari.igvperu.ui.view.windowsize.type.TypeWindowSize
 
 class Information(
     private val windowWidthSizeClass: WindowWidthSizeClass,
-    private val windowHeightSizeClass: WindowHeightSizeClass,
-    private val orientationWindowSize: OrientationWindowSize,
-    private val typeWindowSize: TypeWindowSize
+    private val orientationWindowSize: OrientationWindowSize
 ) {
 
     fun isCompact() = windowWidthSizeClass == WindowWidthSizeClass.Compact
