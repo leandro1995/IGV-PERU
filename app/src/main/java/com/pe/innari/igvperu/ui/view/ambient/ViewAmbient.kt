9339e827b9c07d3682851715d1011efe8b4c1510
leandro1995 <leccbo1995@gmail.com>
2026-09-01 03:13:20 -0500

    :art: ViewAmbient.kt: Implementar soporte para responsividad y tamaños de ventana
    Descripción:
    * Refactorizar la función OnCreate para integrar la lógica de ViewWindowSize
    * Definir métodos específicos para orientaciones vertical, horizontal y dispositivos tablet
    * Establecer PortraitPhone como el método abstracto principal para definir la interfaz de usuario
    * Proveer implementaciones por defecto para configuraciones de pantalla adicionales basadas en la vista vertical o base

--- app/src/main/java/com/pe/innari/igvperu/ui/view/ambient/ViewAmbient.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/ambient/ViewAmbient.kt
@@ -2,6 +2,7 @@ package com.pe.innari.igvperu.ui.view.ambient
 
 import androidx.compose.runtime.Composable
 import com.pe.innari.igvperu.ui.theme.IGVPERUTheme
+import com.pe.innari.igvperu.ui.view.windowsize.ViewWindowSize
 
 /**
  * Clase abstracta base para definir vistas que requieren una estructura común y la aplicación de un tema.
@@ -10,11 +11,43 @@ import com.pe.innari.igvperu.ui.theme.IGVPERUTheme
  */
 abstract class ViewAmbient {
 
+    private val viewWindowSize = ViewWindowSize()
+
     /**
      * Función abstracta que debe ser implementada por las subclases para definir el contenido de la interfaz de usuario Composable.
      */
     @Composable
-    abstract fun OnCreate()
+    fun OnCreate() {
+        viewWindowSize.TypeWindowSize(
+            portraitPhone = { PortraitPhone() },
+            landScapePhone = { LandScapePhone() },
+            medium = { Medium() },
+            portraitTablet = { PortraitTablet() },
+            landScapeTablet = { LandScapeTablet() })
+    }
+
+    @Composable
+    abstract fun PortraitPhone()
+
+    @Composable
+    protected open fun LandScapePhone() {
+        PortraitPhone()
+    }
+
+    @Composable
+    protected open fun Medium() {
+        PortraitPhone()
+    }
+
+    @Composable
+    protected open fun PortraitTablet() {
+        PortraitPhone()
+    }
+
+    @Composable
+    protected open fun LandScapeTablet() {
+        PortraitPhone()
+    }
 
     /**
      * Implementación por defecto para previsualizar la vista.
