a065516dc2518b01cc1f1551edaf0e2fdd5a8f2a
leandro1995 <leccbo1995@gmail.com>
2026-09-01 20:43:26 -0500

    :recycle: TypeWindowSize.kt: Refactorizar los tipos de tamaño de ventana a Compact, Medium y Expanded

--- app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/type/TypeWindowSize.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/view/windowsize/type/TypeWindowSize.kt
@@ -1,5 +1,5 @@
 package com.pe.innari.igvperu.ui.view.windowsize.type
 
 enum class TypeWindowSize {
-    PORTRAIT_PHONE, LANDSCAPE_PHONE, PORTRAIT_TABLET, LANDSCAPE_TABLET
+    COMPACT, MEDIUM, EXPANDED
 }
