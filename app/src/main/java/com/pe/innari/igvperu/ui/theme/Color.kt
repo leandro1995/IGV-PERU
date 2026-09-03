ec07f0978a3fe66e6277308512c23a3039fa0c10
leandro1995 <leccbo1995@gmail.com>
2026-09-02 20:31:05 -0500

    :lipstick: Color.kt: Actualizar la paleta de colores del tema
    Descripción:
    * Eliminar los colores predeterminados de Material 3
    * Agregar nuevas constantes de color para tonos Teal, Neutral, Pink y Red
    * Definir la paleta cromática según la identidad visual del proyecto

--- app/src/main/java/com/pe/innari/igvperu/ui/theme/Color.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/theme/Color.kt
@@ -2,10 +2,27 @@ package com.pe.innari.igvperu.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Neutral10 = Color(0xFF191C1C)
+val Neutral90 = Color(0xFFE4E6E7)
+val Neutral92 = Color(0xFFEDEEEE)
+val Neutral95 = Color(0xFFF4F5F5)
+val Neutral87 = Color(0xFFDCDFDF)
+val Neutral98 = Color(0xFFFBFBFB)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Teal10 = Color(0xFF123435)
+val Teal40 = Color(0xFF1E8085)
+val Teal95 = Color(0xFFE3F1F2)
+
+val TealGrey30 = Color(0xFF434E4E)
+val TealGrey40 = Color(0xFF5F6B6D)
+val TealGrey50 = Color(0xFF6F7F80)
+val TealGrey80 = Color(0xFFC1C8C8)
+val TealGrey90 = Color(0xFFEAEBEB)
+
+val Pink40 = Color(0xFF854C55)
+val Pink90 = Color(0xFFEEE7E8)
+
+val White100 = Color(0xFFFFFFFF)
+
+val Red40 = Color(0xFFB3261E)
+val Red90 = Color(0xFFF9DEDC)
