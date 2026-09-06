51cd44f6753c1d52609418e23dc09219505ed503
leandro1995 <leccbo1995@gmail.com>
2026-09-06 00:44:01 -0500

    :sparkles: ToolBarComponent.kt: Implementar la estructura base del componente
    Descripción:
    * Heredar de la clase base ComponentAmbient para integrar el componente
    * Implementar el método OnCreate con la anotación @Composable
    * Agregar las importaciones necesarias para el funcionamiento de Compose

--- app/src/main/java/com/pe/innari/igvperu/ui/component/ToolBarComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/ToolBarComponent.kt
@@ -1,3 +1,12 @@
 package com.pe.innari.igvperu.ui.component
 
-class ToolBarComponent
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+
+class ToolBarComponent : ComponentAmbient() {
+
+    @Composable
+    override fun OnCreate() {
+
+    }
+}
