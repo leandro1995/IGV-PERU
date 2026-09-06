46ce641d889da1b7d13fc1d44185791944c2ccf8
leandro1995 <leccbo1995@gmail.com>
2026-09-06 02:18:42 -0500

    :sparkles: ToolBarComponentGallery.kt: Implementar el componente ToolBar en la previsualización de la galería

--- app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarComponentGallery.kt
@@ -1,14 +1,17 @@
 package com.pe.innari.igvperu.gallery
 
 import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.component.ToolBarComponent
 import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
 
 class ToolBarComponentGallery : ViewAmbient() {
 
+    private val toolBarComponent = ToolBarComponent()
+
     @Composable
     override fun PortraitCompact() {
-
+        toolBarComponent.OnCreate()
     }
 
     @ThemeAdaptivePreview
