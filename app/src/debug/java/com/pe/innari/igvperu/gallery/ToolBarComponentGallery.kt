81382f75eb6cfb6220a4018eaf68c7c5080c25be
leandro1995 <leccbo1995@gmail.com>
2026-09-06 04:38:44 -0500

    :recycle: ToolBarComponentGallery.kt: Actualizar la ruta de importación de ToolBarComponent

--- app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarComponentGallery.kt
@@ -2,7 +2,7 @@ package com.pe.innari.igvperu.gallery
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import com.pe.innari.igvperu.ui.component.ToolBarComponent
+import com.pe.innari.igvperu.ui.component.toolbar.ToolBarComponent
 import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
 
