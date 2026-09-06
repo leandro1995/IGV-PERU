dbaa0daa676cf391b702e930b9a4ccf038624793
leandro1995 <leccbo1995@gmail.com>
2026-09-06 17:49:47 -0500

    :lipstick: Dimen.kt: Agregar constantes de dimensiones y tamaños de texto

--- app/src/main/java/com/pe/innari/igvperu/ui/theme/Dimen.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/theme/Dimen.kt
@@ -1,7 +1,13 @@
 package com.pe.innari.igvperu.ui.theme
 
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+val Dimen1 = 1.dp
 val Dimen20 = 20.dp
 val Dimen24 = 24.dp
 val Dimen76 = 76.dp
+
+
+val Text12 = 12.sp
+val Text20 = 20.sp
