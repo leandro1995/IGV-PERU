c3c082ba28a5132b261f9bcd5897a57ebc1b9e3a
leandro1995 <leccbo1995@gmail.com>
2026-09-06 12:03:40 -0500

    :sparkles: ToolBarComponentGallery.kt: Agregar variantes del componente de barra de herramientas
    Descripción:
    * Incluir previsualizaciones para la barra de herramientas con y sin ícono
    * Configurar espaciado y márgenes en el contenedor de la galería
    * Implementar el uso del modelo ToolBar para definir las propiedades del componente

--- app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarComponentGallery.kt
@@ -1,19 +1,35 @@
 package com.pe.innari.igvperu.gallery
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.pe.innari.igvperu.ui.component.toolbar.ToolBarComponent
+import com.pe.innari.igvperu.ui.component.toolbar.model.ToolBar
 import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
 
 class ToolBarComponentGallery : ViewAmbient() {
 
-    private val toolBarComponent = ToolBarComponent()
+    private val toolBarNoIconComponent =
+        ToolBarComponent(toolBar = ToolBar(title = "title", subTitle = "subTitle"))
+
+    private val toolBarWithIconComponent = ToolBarComponent(
+        toolBar = ToolBar(
+            icon = android.R.drawable.star_on, title = "title", subTitle = "subTitle"
+        )
+    )
 
     @Composable
     override fun PortraitCompact() {
-        Column {
-            toolBarComponent.OnCreate()
+        Column(
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            toolBarNoIconComponent.OnCreate()
+            toolBarWithIconComponent.OnCreate()
         }
     }
 
