cba399c925422cc1d6d27e9a55d054babbaba0a7
leandro1995 <leccbo1995@gmail.com>
2026-09-06 04:35:30 -0500

    :lipstick: ToolBarComponent.kt: Mejorar el diseño y la alineación del componente
    Descripción:
    * Configurar una altura fija y rellenos horizontales en el contenedor principal
    * Alinear verticalmente los elementos dentro de la fila
    * Ajustar el tamaño de la imagen y el espaciado de la columna de texto
    * Incorporar el uso de constantes de dimensiones para mantener la consistencia visual

--- app/src/main/java/com/pe/innari/igvperu/ui/component/ToolBarComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/ToolBarComponent.kt
@@ -3,26 +3,46 @@ package com.pe.innari.igvperu.ui.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.pe.innari.igvperu.R
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+import com.pe.innari.igvperu.ui.theme.Dimen20
+import com.pe.innari.igvperu.ui.theme.Dimen24
+import com.pe.innari.igvperu.ui.theme.Dimen76
 
 class ToolBarComponent : ComponentAmbient() {
 
     @Composable
     override fun OnCreate() {
-        OutlinedCard(modifier = Modifier.fillMaxWidth()) {
-            Row {
+        OutlinedCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(Dimen76)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = Dimen20),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 Image(
+                    modifier = Modifier.size(Dimen24),
                     painter = painterResource(R.drawable.ic_launcher_background),
                     contentDescription = null
                 )
-                Column {
+                Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = Dimen20, end = Dimen20)) {
                     Text(text = "1")
                     Text(text = "2")
                 }
