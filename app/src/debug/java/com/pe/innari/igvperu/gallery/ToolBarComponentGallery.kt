a6fe6762b080c84619ccb7d7dbdebcc3d4657782
leandro1995 <leccbo1995@gmail.com>
2026-09-06 02:13:34 -0500

    :sparkles: ToolBarComponentGallery.kt: Implementar la estructura base para la previsualización del Toolbar
    Descripción:
    * Heredar de la clase `ViewAmbient` para la integración en la galería
    * Implementar el método `PortraitCompact` para la configuración del diseño vertical
    * Configurar el método `Preview` con soporte para temas adaptativos

--- app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/ToolBarComponentGallery.kt
@@ -1,3 +1,19 @@
 package com.pe.innari.igvperu.gallery
 
-class ToolBarComponentGallery
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
+import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
+
+class ToolBarComponentGallery : ViewAmbient() {
+
+    @Composable
+    override fun PortraitCompact() {
+
+    }
+
+    @ThemeAdaptivePreview
+    @Composable
+    override fun Preview() {
+        super.Preview()
+    }
+}
