34d2c9ef65e697e9f01dae6af3db468126bbe1f4
leandro1995 <leccbo1995@gmail.com>
2026-09-06 11:42:29 -0500

    :sparkles: ToolBar.kt: Actualizar el modelo ToolBar con métodos auxiliares
    Descripción:
    * Cambiar la visibilidad de las propiedades title y subTitle a pública
    * Implementar el método hasIcon para verificar la presencia de un icono
    * Agregar el método icon para obtener el recurso del icono de forma segura

--- app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/model/ToolBar.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/toolbar/model/ToolBar.kt
@@ -1,5 +1,10 @@
 package com.pe.innari.igvperu.ui.component.toolbar.model
 
 class ToolBar(
-    private val icon: Int? = null, private val title: String, private val subTitle: String
-)
+    private val icon: Int? = null, val title: String, val subTitle: String
+) {
+
+    fun hasIcon() = icon != null
+
+    fun icon() = icon ?: 0
+}
