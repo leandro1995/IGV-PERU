c7c46460f90239b9813915a8745d5d95f0175ec2
leandro1995 <leccbo1995@gmail.com>
2026-09-06 02:41:07 -0500

    :sparkles: ToolBarComponent.kt: Implementar la estructura visual del componente
    Descripción:
    * Agregar un contenedor `OutlinedCard` para definir el diseño de la barra de herramientas
    * Incluir una disposición de fila que contiene una imagen y una columna con textos
    * Importar y configurar los elementos necesarios de Jetpack Compose Material 3

--- app/src/main/java/com/pe/innari/igvperu/ui/component/ToolBarComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/ToolBarComponent.kt
@@ -1,12 +1,32 @@
 package com.pe.innari.igvperu.ui.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.pe.innari.igvperu.R
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
 
 class ToolBarComponent : ComponentAmbient() {
 
     @Composable
     override fun OnCreate() {
-
+        OutlinedCard(modifier = Modifier.fillMaxWidth()) {
+            Row {
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = null
+                )
+                Column {
+                    Text(text = "1")
+                    Text(text = "2")
+                }
+            }
+        }
     }
 }
