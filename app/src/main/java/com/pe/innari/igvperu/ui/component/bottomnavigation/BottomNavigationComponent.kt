4cc9cb17371614b1929ca14bbfc96712552fecd9
leandro1995 <leccbo1995@gmail.com>
2026-09-07 02:57:32 -0500

    :recycle: BottomNavigationComponent.kt: Actualizar constructor para incluir TypeBottomNavigation

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
@@ -2,8 +2,10 @@ package com.pe.innari.igvperu.ui.component.bottomnavigation
 
 import androidx.compose.runtime.Composable
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+import com.pe.innari.igvperu.ui.component.bottomnavigation.type.TypeBottomNavigation
 
-class BottomNavigationComponent : ComponentAmbient() {
+class BottomNavigationComponent(private val typeBottomNavigation: TypeBottomNavigation) :
+    ComponentAmbient() {
 
     @Composable
     override fun OnCreate() {
