a419e06da3591317b5a1d13c10378f9bac80b448
leandro1995 <leccbo1995@gmail.com>
2026-09-11 19:37:24 -0500

    :sparkles: BottomNavigationComponentGallery.kt: Implementar previsualizaciones adaptativas del componente de navegación
    Descripción:
    * Configurar el componente de navegación para orientaciones Portrait y Landscape
    * Definir el comportamiento en tamaños de pantalla Compact, Medium y Expanded
    * Asignar el tipo de navegación vertical u horizontal según la configuración de la vista
    * Agregar elementos de texto como marcadores de posición para validar el contenido en la galería

--- app/src/debug/java/com/pe/innari/igvperu/gallery/BottomNavigationComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/BottomNavigationComponentGallery.kt
@@ -1,6 +1,9 @@
 package com.pe.innari.igvperu.gallery
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.component.bottomnavigation.BottomNavigationComponent
+import com.pe.innari.igvperu.ui.component.bottomnavigation.type.TypeBottomNavigation
 import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
 
@@ -8,7 +11,44 @@ class BottomNavigationComponentGallery : ViewAmbient() {
 
     @Composable
     override fun PortraitCompact() {
+        BottomNavigationComponent(typeBottomNavigation = TypeBottomNavigation.VERTICAL).OnCreate {
+            Text("PortraitCompact")
+        }
+    }
 
+    @Composable
+    override fun PortraitMedium() {
+        BottomNavigationComponent(typeBottomNavigation = TypeBottomNavigation.HORIZONTAL).OnCreate {
+            Text("PortraitMedium")
+        }
+    }
+
+    @Composable
+    override fun PortraitExpanded() {
+        BottomNavigationComponent(typeBottomNavigation = TypeBottomNavigation.HORIZONTAL).OnCreate {
+            Text("PortraitMedium")
+        }
+    }
+
+    @Composable
+    override fun LandScapeCompact() {
+        BottomNavigationComponent(typeBottomNavigation = TypeBottomNavigation.HORIZONTAL).OnCreate {
+            Text("LandScapeCompact")
+        }
+    }
+
+    @Composable
+    override fun LandScapeMedium() {
+        BottomNavigationComponent(typeBottomNavigation = TypeBottomNavigation.HORIZONTAL).OnCreate {
+            Text("LandScapeMedium")
+        }
+    }
+
+    @Composable
+    override fun LandScapeExpanded() {
+        BottomNavigationComponent(typeBottomNavigation = TypeBottomNavigation.HORIZONTAL).OnCreate {
+            Text("LandScapeExpanded")
+        }
     }
 
     @ThemeAdaptivePreview
