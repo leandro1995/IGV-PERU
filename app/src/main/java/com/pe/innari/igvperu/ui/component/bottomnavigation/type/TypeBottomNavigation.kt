8cb6dbb5fe656ce463ab092eafc1ba5662cdacef
leandro1995 <leccbo1995@gmail.com>
2026-09-12 04:09:28 -0500

    :memo: TypeBottomNavigation.kt: Documentar el enum TypeBottomNavigation y sus valores de navegación

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/type/TypeBottomNavigation.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/type/TypeBottomNavigation.kt
@@ -1,5 +1,18 @@
 package com.pe.innari.igvperu.ui.component.bottomnavigation.type
 
+import com.pe.innari.igvperu.ui.component.bottomnavigation.BottomNavigationComponent
+
+/**
+ * Define los tipos de navegación disponibles para el componente [BottomNavigationComponent].
+ */
 enum class TypeBottomNavigation {
-    BOTTOM, RAIL
+    /**
+     * Barra de navegación estándar en la parte inferior de la pantalla.
+     */
+    BOTTOM,
+
+    /**
+     * Riel de navegación lateral, generalmente utilizado en dispositivos de pantalla ancha.
+     */
+    RAIL
 }
