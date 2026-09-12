bc95e4c8cc53fc2c5229d73eb23f1481640e8ab6
leandro1995 <leccbo1995@gmail.com>
2026-09-11 19:37:39 -0500

    :sparkles: BottomNavigationComponent.kt: Implementar el tipo de navegación horizontal utilizando NavigationRail

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
@@ -1,11 +1,17 @@
 package com.pe.innari.igvperu.ui.component.bottomnavigation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
 import com.pe.innari.igvperu.ui.component.bottomnavigation.type.TypeBottomNavigation
 
@@ -20,7 +26,7 @@ class BottomNavigationComponent(private val typeBottomNavigation: TypeBottomNavi
             }
 
             TypeBottomNavigation.HORIZONTAL -> {
-
+                BottomNavigationHorizontal(view = view)
             }
         }
     }
@@ -29,6 +35,25 @@ class BottomNavigationComponent(private val typeBottomNavigation: TypeBottomNavi
     private fun BottomNavigationVertical(view: @Composable () -> Unit) {
         Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {}) { paddingValues ->
             Column(modifier = Modifier.padding(paddingValues)) { view() }
+        }
+    }
+
+    @Composable
+    private fun BottomNavigationHorizontal(
+        containerColor: Color = MaterialTheme.colorScheme.background,
+        contentColor: Color = contentColorFor(containerColor),
+        view: @Composable () -> Unit
+    ) {
+        Row(modifier = Modifier.fillMaxSize()) {
+            NavigationRail(
+                containerColor = containerColor, contentColor = contentColor
+            ) {
+                Box(
+                    modifier = Modifier
+                        .weight(1F)
+                        .fillMaxSize()
+                ) { view() }
+            }
         }
     }
 }
