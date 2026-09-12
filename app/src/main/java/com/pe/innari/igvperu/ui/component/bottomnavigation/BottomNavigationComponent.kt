2388fcecf8aa6b65c36298611f4ebd2b96a18786
leandro1995 <leccbo1995@gmail.com>
2026-09-11 19:25:08 -0500

    :sparkles: BottomNavigationComponent.kt: Implementar la lógica de navegación vertical
    Descripción:
    * Actualizar la función OnCreate para recibir una vista composable como parámetro
    * Implementar la estructura de navegación vertical utilizando Scaffold y Column
    * Agregar lógica para manejar los diferentes tipos de navegación definidos en TypeBottomNavigation

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
@@ -1,6 +1,11 @@
 package com.pe.innari.igvperu.ui.component.bottomnavigation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
 import com.pe.innari.igvperu.ui.component.bottomnavigation.type.TypeBottomNavigation
 
@@ -8,7 +13,22 @@ class BottomNavigationComponent(private val typeBottomNavigation: TypeBottomNavi
     ComponentAmbient() {
 
     @Composable
-    override fun OnCreate() {
+    override fun OnCreate(view: @Composable (() -> Unit)) {
+        when (typeBottomNavigation) {
+            TypeBottomNavigation.VERTICAL -> {
+                BottomNavigationVertical(view = view)
+            }
 
+            TypeBottomNavigation.HORIZONTAL -> {
+
+            }
+        }
+    }
+
+    @Composable
+    private fun BottomNavigationVertical(view: @Composable () -> Unit) {
+        Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {}) { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) { view() }
+        }
     }
 }
