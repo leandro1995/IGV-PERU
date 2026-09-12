1b49da46d6f39764a82a15caf0d21c4aa69b15a9
leandro1995 <leccbo1995@gmail.com>
2026-09-12 04:09:43 -0500

    :memo: ItemBottomNavigation.kt: Documentar la clase y sus propiedades

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/model/ItemBottomNavigation.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/model/ItemBottomNavigation.kt
@@ -1,5 +1,11 @@
 package com.pe.innari.igvperu.ui.component.bottomnavigation.model
 
+/**
+ * Representa un ítem individual dentro de la navegación.
+ *
+ * @property icon Recurso de dibujo (Drawable Res) para el icono del ítem.
+ * @property title Etiqueta de texto que se mostrará debajo o junto al icono.
+ */
 class ItemBottomNavigation(
     val icon: Int, val title: String
 )
