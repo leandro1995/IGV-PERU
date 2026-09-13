31e78dd5604b12b9cde60e78ee2f79d10564cfaa
leandro1995 <leccbo1995@gmail.com>
2026-09-13 00:30:13 -0500

    :lipstick: TabComponeGallery.kt: Agregar Scaffold y Column para mejorar el diseño en PortraitCompact

--- app/src/debug/java/com/pe/innari/igvperu/gallery/TabComponeGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/TabComponeGallery.kt
@@ -1,6 +1,12 @@
 package com.pe.innari.igvperu.gallery
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.pe.innari.igvperu.ui.component.tab.TabComponent
 import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
@@ -9,7 +15,15 @@ class TabComponeGallery : ViewAmbient<Any>() {
 
     @Composable
     override fun PortraitCompact() {
-        TabComponent().OnCreate()
+        Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxWidth()
+            ) {
+                TabComponent().OnCreate()
+            }
+        }
     }
 
     @ThemeAdaptivePreview
