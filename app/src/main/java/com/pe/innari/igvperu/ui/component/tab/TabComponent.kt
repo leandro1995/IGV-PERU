9b454b1feb8c0104616c94f1b0456f72a5e78953
leandro1995 <leccbo1995@gmail.com>
2026-09-13 02:05:50 -0500

    :lipstick: TabComponent.kt: Ajustar la alineación y el diseño del componente de pestaña
    Descripción:
    * Configurar una altura fija para el contenedor principal utilizando Dimen56
    * Centrar horizontal y verticalmente el contenido dentro de la columna
    * Actualizar el modificador de la columna para ocupar todo el espacio disponible mediante fillMaxSize

--- app/src/main/java/com/pe/innari/igvperu/ui/component/tab/TabComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/tab/TabComponent.kt
@@ -1,19 +1,32 @@
 package com.pe.innari.igvperu.ui.component.tab
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+import com.pe.innari.igvperu.ui.theme.Dimen56
 
 class TabComponent : ComponentAmbient() {
 
     @Composable
     override fun OnCreate() {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Column(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(Dimen56)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
                 Text(text = "DEMO")
             }
         }
