63b4e1dae181c021769b362a9a000515c1c52c2a
leandro1995 <leccbo1995@gmail.com>
2026-09-13 00:30:40 -0500

    :sparkles: TabComponent.kt: Implementar estructura básica de la interfaz

--- app/src/main/java/com/pe/innari/igvperu/ui/component/tab/TabComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/tab/TabComponent.kt
@@ -1,12 +1,21 @@
 package com.pe.innari.igvperu.ui.component.tab
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
 
 class TabComponent : ComponentAmbient() {
 
     @Composable
     override fun OnCreate() {
-        
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Text(text = "DEMO")
+            }
+        }
     }
 }
