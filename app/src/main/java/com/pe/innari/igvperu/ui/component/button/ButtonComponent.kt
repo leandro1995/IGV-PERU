0d867810bd078999bf2939e18e2bdec6cd40c3c1
leandro1995 <leccbo1995@gmail.com>
2026-09-13 11:29:14 -0500

    :sparkles: ButtonComponent.kt: Implementar la estructura inicial y herencia de ComponentAmbient

--- app/src/main/java/com/pe/innari/igvperu/ui/component/button/ButtonComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/button/ButtonComponent.kt
@@ -1,3 +1,12 @@
 package com.pe.innari.igvperu.ui.component.button
 
-class ButtonComponent
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+
+class ButtonComponent : ComponentAmbient() {
+
+    @Composable
+    override fun OnCreate() {
+
+    }
+}
