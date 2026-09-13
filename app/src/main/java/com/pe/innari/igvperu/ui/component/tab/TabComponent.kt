2004fde29eeb92e4e6f2685d05f9b43049c01c83
leandro1995 <leccbo1995@gmail.com>
2026-09-12 23:05:51 -0500

    :sparkles: TabComponent.kt: Implementar la estructura base heredando de ComponentAmbient

--- app/src/main/java/com/pe/innari/igvperu/ui/component/tab/TabComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/tab/TabComponent.kt
@@ -1,3 +1,12 @@
 package com.pe.innari.igvperu.ui.component.tab
 
-class TabComponent
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+
+class TabComponent : ComponentAmbient() {
+
+    @Composable
+    override fun OnCreate() {
+        
+    }
+}
