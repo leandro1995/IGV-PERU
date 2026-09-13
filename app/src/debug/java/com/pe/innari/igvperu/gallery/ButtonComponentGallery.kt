404f18716fc6e783ca0f50b7b405efd48af8f154
leandro1995 <leccbo1995@gmail.com>
2026-09-13 18:31:13 -0500

    :sparkles: ButtonComponentGallery.kt: Actualizar la galería de botones
    Descripción:
    * Agregar la variante de botón destructivo a la previsualización
    * Implementar espaciado vertical entre los componentes de la galería

--- app/src/debug/java/com/pe/innari/igvperu/gallery/ButtonComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/ButtonComponentGallery.kt
@@ -1,11 +1,13 @@
 package com.pe.innari.igvperu.gallery
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.pe.innari.igvperu.ui.component.button.ButtonComponent
 import com.pe.innari.igvperu.ui.component.button.model.Button
 import com.pe.innari.igvperu.ui.component.button.type.ButtonType
@@ -18,6 +20,7 @@ class ButtonComponentGallery : ViewAmbient<Any>() {
     override fun PortraitCompact() {
         Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
             Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
                 modifier = Modifier
                     .padding(paddingValues)
                     .fillMaxSize()
@@ -25,6 +28,7 @@ class ButtonComponentGallery : ViewAmbient<Any>() {
                 ButtonComponent(buttonType = ButtonType.PRIMARY, button = button()).OnCreate()
                 ButtonComponent(buttonType = ButtonType.SECONDARY, button = button()).OnCreate()
                 ButtonComponent(buttonType = ButtonType.OUTLINED, button = button()).OnCreate()
+                ButtonComponent(buttonType = ButtonType.DESTRUCTIVE, button = button()).OnCreate()
             }
         }
     }
