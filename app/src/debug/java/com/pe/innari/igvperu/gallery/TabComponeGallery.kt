24c6c7d0c5e813ecb6dd2667596c752323f7b707
leandro1995 <leccbo1995@gmail.com>
2026-09-12 23:35:49 -0500

    :sparkles: TabComponeGallery.kt: Implementar la visualización de TabComponent

--- app/src/debug/java/com/pe/innari/igvperu/gallery/TabComponeGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/TabComponeGallery.kt
@@ -1,13 +1,15 @@
 package com.pe.innari.igvperu.gallery
 
 import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.component.tab.TabComponent
 import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
 
 class TabComponeGallery : ViewAmbient<Any>() {
+
     @Composable
     override fun PortraitCompact() {
-
+        TabComponent().OnCreate()
     }
 
     @ThemeAdaptivePreview
