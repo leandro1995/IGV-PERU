efe97e9ea50235e8f5d06fa0724e1145554d158d
leandro1995 <leccbo1995@gmail.com>
2026-09-12 20:13:05 -0500

    :memo: BottomNavigationCallBack.kt: Documentar la interfaz de callback de navegación

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/callback/BottomNavigationCallBack.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/callback/BottomNavigationCallBack.kt
@@ -1,5 +1,13 @@
 package com.pe.innari.igvperu.ui.component.bottomnavigation.callback
 
+/**
+ * Interfaz de callback para eventos de navegación en la barra inferior o riel.
+ */
 interface BottomNavigationCallBack {
+    /**
+     * Se invoca cuando se selecciona un nuevo elemento de navegación.
+     *
+     * @param position El índice del elemento seleccionado.
+     */
     fun position(position: Int)
 }
