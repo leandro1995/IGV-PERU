507bda502f21e5b37c0603c3197d70ea11e6654c
leandro1995 <leccbo1995@gmail.com>
2026-09-12 20:12:52 -0500

    :memo: AppExtension.kt: Documentar la función de extensión orEmpty para tipos enteros

--- app/src/main/java/com/pe/innari/igvperu/extension/AppExtension.kt
+++ app/src/main/java/com/pe/innari/igvperu/extension/AppExtension.kt
@@ -1,3 +1,8 @@
 package com.pe.innari.igvperu.extension
 
+/**
+ * Retorna el valor del entero o 0 si es nulo.
+ *
+ * @return El valor del entero si no es nulo, de lo contrario 0.
+ */
 fun Int?.orEmpty() = this ?: 0
