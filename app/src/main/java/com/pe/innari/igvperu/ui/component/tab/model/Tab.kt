e048c38dd624bba4d7b42ef4053ab222701504ca
leandro1995 <leccbo1995@gmail.com>
2026-09-13 03:27:41 -0500

    :art: Tab.kt: Refactorizar a data class y agregar documentación

--- app/src/main/java/com/pe/innari/igvperu/ui/component/tab/model/Tab.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/tab/model/Tab.kt
@@ -2,4 +2,10 @@ package com.pe.innari.igvperu.ui.component.tab.model
 
 import androidx.annotation.DrawableRes
 
-class Tab(@DrawableRes val icon: Int, val label: String)
+/**
+ * Representa la información de una pestaña (tab) individual.
+ *
+ * @property icon Recurso de dibujo (drawable) que se mostrará como icono de la pestaña.
+ * @property label Texto descriptivo que se mostrará en la pestaña.
+ */
+data class Tab(@DrawableRes val icon: Int, val label: String)
