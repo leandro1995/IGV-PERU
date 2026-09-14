86a4c72ee7b40a02c595a8c0ca69337f6411b82f
leandro1995 <leccbo1995@gmail.com>
2026-09-14 15:41:43 -0500

    :sparkles: EditTextFormatUtil.kt: Implementar validación y formateo numérico para campos de texto
    Descripción:
    * Implementar la función numberFormat utilizando InputTransformation
    * Agregar soporte para validación de números enteros y decimales mediante Regex
    * Definir límites de 10 dígitos enteros y 2 decimales para la entrada de texto

--- app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/util/EditTextFormatUtil.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/util/EditTextFormatUtil.kt
@@ -1,3 +1,36 @@
 package com.pe.innari.igvperu.ui.component.edittext.util
 
-object EditTextFormatUtil
+import androidx.compose.foundation.text.input.InputTransformation
+import androidx.compose.foundation.text.input.byValue
+
+object EditTextFormatUtil {
+
+    fun numberFormat(allowDecimal: Boolean): InputTransformation {
+        return InputTransformation.byValue { current, proposed ->
+            when {
+                proposed.isEmpty() -> {
+                    proposed
+                }
+
+                proposed.matches(regex(allowDecimal = allowDecimal)) -> {
+                    proposed
+                }
+
+                else -> {
+                    current
+                }
+            }
+        }
+    }
+
+    private fun regex(allowDecimal: Boolean) = Regex(
+        if (allowDecimal) {
+            """^\d{1,$MAX_INTEGER_DIGITS}(\.\d{0,$MAX_DECIMAL_DIGITS})?$"""
+        } else {
+            """^\d{1,$MAX_INTEGER_DIGITS}$"""
+        }
+    )
+
+    private const val MAX_INTEGER_DIGITS = 10
+    private const val MAX_DECIMAL_DIGITS = 2
+}
