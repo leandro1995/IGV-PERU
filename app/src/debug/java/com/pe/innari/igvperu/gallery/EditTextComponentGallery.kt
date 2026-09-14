a5cbc73d0b763108ab85f058f3e8453890dc449e
leandro1995 <leccbo1995@gmail.com>
2026-09-13 22:12:50 -0500

    :sparkles: EditTextComponentGallery.kt: Implementar la estructura base y previsualización del componente

--- app/src/debug/java/com/pe/innari/igvperu/gallery/EditTextComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/EditTextComponentGallery.kt
@@ -1,4 +1,19 @@
 package com.pe.innari.igvperu.gallery
 
-class EditTextComponentGallery {
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
+import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
+
+class EditTextComponentGallery : ViewAmbient<Any>() {
+
+    @Composable
+    override fun PortraitCompact() {
+        
+    }
+
+    @ThemeAdaptivePreview
+    @Composable
+    override fun Preview() {
+        super.Preview()
+    }
 }
