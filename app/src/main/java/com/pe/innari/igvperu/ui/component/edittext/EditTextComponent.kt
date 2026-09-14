91b45afceb5cf3878b86f5db2b248126baff1561
leandro1995 <leccbo1995@gmail.com>
2026-09-14 00:41:01 -0500

    :sparkles: EditTextComponent.kt: Implementar la interfaz visual del componente de entrada de texto
    Descripción:
    * Implementar el diseño visual mediante OutlinedTextField
    * Configurar la alineación del texto, estilos y bordes redondeados
    * Actualizar el constructor para integrar el modelo de datos EditText
    * Definir la estructura y el comportamiento del marcador de posición (placeholder)

--- app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/EditTextComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/EditTextComponent.kt
@@ -1,12 +1,46 @@
 package com.pe.innari.igvperu.ui.component.edittext
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+import com.pe.innari.igvperu.ui.component.edittext.model.EditText
+import com.pe.innari.igvperu.ui.theme.Dimen68
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
+import com.pe.innari.igvperu.ui.theme.Dimen14
 
-class EditTextComponent : ComponentAmbient() {
+class EditTextComponent(private val editText: EditText) : ComponentAmbient() {
 
     @Composable
     override fun OnCreate() {
-
+        OutlinedTextField(
+            shape = RoundedCornerShape(Dimen14),
+            lineLimits = TextFieldLineLimits.SingleLine,
+            textStyle = LocalTextStyle.current.copy(
+                textAlign = TextAlign.End
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(Dimen68),
+            state = rememberTextFieldState(),
+            placeholder = {
+                Text(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.CenterEnd),
+                    text = editText.placeHolder,
+                    textAlign = TextAlign.End
+                )
+            })
     }
 }
