62292ec9bc37ce31fdc73e0c27918ceb1592e2d7
leandro1995 <leccbo1995@gmail.com>
2026-09-13 23:43:07 -0500

    :lipstick: EditTextComponentGallery.kt: Actualizar la estructura de la vista previa compacta
    Descripción:
    * Implementar Scaffold para ocupar el espacio total de la pantalla
    * Agregar un contenedor Column para gestionar los valores de padding
    * Envolver la inicialización de EditTextComponent en la nueva jerarquía de diseño

--- app/src/debug/java/com/pe/innari/igvperu/gallery/EditTextComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/EditTextComponentGallery.kt
@@ -1,6 +1,11 @@
 package com.pe.innari.igvperu.gallery
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.pe.innari.igvperu.ui.component.edittext.EditTextComponent
 import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
@@ -9,7 +14,11 @@ class EditTextComponentGallery : ViewAmbient<Any>() {
 
     @Composable
     override fun PortraitCompact() {
-        EditTextComponent().OnCreate()
+        Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) {
+                EditTextComponent().OnCreate()
+            }
+        }
     }
 
     @ThemeAdaptivePreview
