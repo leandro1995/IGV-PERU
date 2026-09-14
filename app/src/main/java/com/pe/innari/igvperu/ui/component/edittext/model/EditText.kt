ef887038b84e9800677ba21006bcac23c8c5e5da
leandro1995 <leccbo1995@gmail.com>
2026-09-14 15:41:17 -0500

    :sparkles: EditText.kt: Agregar la propiedad editTextType al modelo

--- app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/model/EditText.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/model/EditText.kt
@@ -1,3 +1,10 @@
 package com.pe.innari.igvperu.ui.component.edittext.model
 
-class EditText(val placeHolder: String = "", val label: String, val isCurrencySymbol: Boolean = false)
+import com.pe.innari.igvperu.ui.component.edittext.type.EditTextType
+
+class EditText(
+    val placeHolder: String = "",
+    val label: String,
+    val isCurrencySymbol: Boolean = false,
+    var editTextType: EditTextType = EditTextType.NONE
+)
