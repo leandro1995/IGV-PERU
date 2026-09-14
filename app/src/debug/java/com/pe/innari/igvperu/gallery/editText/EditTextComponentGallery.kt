3e8e945d4aaca80342be495e8d0bcd8550c8b68d
leandro1995 <leccbo1995@gmail.com>
2026-09-14 18:58:19 -0500

    :sparkles: EditTextComponentGallery.kt: Implementar el manejo de estado en la galería de componentes de texto
    Descripción:
    * Implementar el método InstanceMutableValues para inicializar el estado de los campos de texto
    * Actualizar la herencia de ViewAmbient para utilizar la clase EditTextState
    * Vincular los estados decimal e integer a los componentes EditTextComponent en la previsualización

--- app/src/debug/java/com/pe/innari/igvperu/gallery/editText/EditTextComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/editText/EditTextComponentGallery.kt
@@ -4,18 +4,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.pe.innari.igvperu.extension.orEmpty
+import com.pe.innari.igvperu.gallery.editText.state.EditTextState
 import com.pe.innari.igvperu.ui.component.edittext.EditTextComponent
 import com.pe.innari.igvperu.ui.component.edittext.model.EditText
 import com.pe.innari.igvperu.ui.component.edittext.type.EditTextType
 import com.pe.innari.igvperu.ui.view.adaptivepreview.ThemeAdaptivePreview
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
 
-class EditTextComponentGallery : ViewAmbient<Any>() {
+class EditTextComponentGallery : ViewAmbient<EditTextState>() {
+
+    @Composable
+    override fun InstanceMutableValues() {
+        mutableValues = EditTextState(
+            decimal = rememberTextFieldState(), integer = rememberTextFieldState()
+        )
+    }
 
     @Composable
     override fun PortraitCompact() {
@@ -30,17 +40,15 @@ class EditTextComponentGallery : ViewAmbient<Any>() {
                         label = "label",
                         isCurrencySymbol = true,
                         editTextType = EditTextType.DECIMAL
-                    ),
-                    trailingIcon = {
+                    ), trailingIcon = {
                         Text("AUTO")
-                    }
+                    }, textFieldState = mutableValues?.decimal.orEmpty()
                 ).OnCreate()
 
                 EditTextComponent(
                     editText = EditText(
-                        placeHolder = "placeHolder",
-                        label = "label"
-                    )
+                        placeHolder = "placeHolder", label = "label"
+                    ), textFieldState = mutableValues?.integer.orEmpty()
                 ).OnCreate()
             }
         }
