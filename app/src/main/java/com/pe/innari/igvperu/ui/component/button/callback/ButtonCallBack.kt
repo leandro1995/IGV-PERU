318916b5fc6d60362d355436d3588a9ab51ff1fc
leandro1995 <leccbo1995@gmail.com>
2026-09-13 19:21:59 -0500

    :memo: ButtonCallBack.kt: Documentar la interfaz y su método de clic

--- app/src/main/java/com/pe/innari/igvperu/ui/component/button/callback/ButtonCallBack.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/button/callback/ButtonCallBack.kt
@@ -1,6 +1,12 @@
 package com.pe.innari.igvperu.ui.component.button.callback
 
+/**
+ * Interfaz para manejar los eventos de interacción con los botones.
+ */
 interface ButtonCallBack {
-    
+
+    /**
+     * Se invoca cuando el usuario realiza un clic en el botón.
+     */
     fun onClick()
 }
