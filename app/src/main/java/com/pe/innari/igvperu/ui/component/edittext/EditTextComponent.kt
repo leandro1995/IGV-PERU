3147cb7736cdad29aa56e439aec32f4f1fd7e41e
leandro1995 <leccbo1995@gmail.com>
2026-09-14 12:42:36 -0500

    :sparkles: EditTextComponent.kt: Agregar soporte para icono final al componente
    Descripción:
    * Agregar el parámetro trailingIcon al constructor del componente
    * Implementar la visualización del icono final en la interfaz
    * Eliminar importaciones de diseño no utilizadas

--- app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/EditTextComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/EditTextComponent.kt
@@ -1,13 +1,11 @@
 package com.pe.innari.igvperu.ui.component.edittext
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.input.TextFieldLineLimits
@@ -32,7 +30,10 @@ import com.pe.innari.igvperu.ui.theme.Dimen15
 import com.pe.innari.igvperu.ui.theme.Dimen55
 import com.pe.innari.igvperu.ui.theme.Dimen68
 
-class EditTextComponent(private val editText: EditText) : ComponentAmbient() {
+class EditTextComponent(
+    private val editText: EditText,
+    private val trailingIcon: @Composable (() -> Unit)? = null
+) : ComponentAmbient() {
 
     @Composable
     override fun OnCreate() {
@@ -55,7 +56,9 @@ class EditTextComponent(private val editText: EditText) : ComponentAmbient() {
                     textAlign = TextAlign.End
                 )
             },
-            leadingIcon = { LeadingIcon() })
+            leadingIcon = { LeadingIcon() },
+            trailingIcon = trailingIcon
+        )
     }
 
     @Composable
