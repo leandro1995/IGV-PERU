ebd5e6318b415d2aa38a901fdf8f8f3b375511f9
leandro1995 <leccbo1995@gmail.com>
2026-09-13 19:22:11 -0500

    :memo: Button.kt: Agregar documentación a la clase Button

--- app/src/main/java/com/pe/innari/igvperu/ui/component/button/model/Button.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/button/model/Button.kt
@@ -2,4 +2,10 @@ package com.pe.innari.igvperu.ui.component.button.model
 
 import androidx.annotation.DrawableRes
 
+/**
+ * Modelo de datos que representa la configuración de un botón.
+ *
+ * @property icon Recurso drawable opcional para mostrar un icono al lado del texto.
+ * @property label Texto descriptivo que se mostrará en el botón.
+ */
 class Button(@DrawableRes val icon: Int? = null, val label: String)
