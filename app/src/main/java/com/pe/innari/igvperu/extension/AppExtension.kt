f4878e9f800ba67c50f089822f8b4620a6e0452e
leandro1995 <leccbo1995@gmail.com>
2026-09-14 18:57:03 -0500

    :sparkles: AppExtension.kt: Agregar función de extensión para TextFieldState

--- app/src/main/java/com/pe/innari/igvperu/extension/AppExtension.kt
+++ app/src/main/java/com/pe/innari/igvperu/extension/AppExtension.kt
@@ -1,8 +1,12 @@
 package com.pe.innari.igvperu.extension
 
+import androidx.compose.foundation.text.input.TextFieldState
+
 /**
  * Retorna el valor del entero o 0 si es nulo.
  *
  * @return El valor del entero si no es nulo, de lo contrario 0.
  */
 fun Int?.orEmpty() = this ?: 0
+
+fun TextFieldState?.orEmpty() = this ?: TextFieldState()
