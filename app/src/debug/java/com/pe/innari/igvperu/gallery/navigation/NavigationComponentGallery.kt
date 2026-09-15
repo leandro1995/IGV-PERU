ac2bbd71056f9bd90581ea61d45c307b1f24a43d
leandro1995 <leccbo1995@gmail.com>
2026-09-15 02:08:25 -0500

    :sparkles: NavigationComponentGallery.kt: Implementar la estructura base y herencia del componente
    Descripción:
    * Heredar de la clase base ViewAmbient
    * Implementar el método PortraitCompact con la anotación @Composable
    * Agregar los imports necesarios para el funcionamiento de la galería

--- app/src/debug/java/com/pe/innari/igvperu/gallery/navigation/NavigationComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/navigation/NavigationComponentGallery.kt
@@ -1,3 +1,13 @@
 package com.pe.innari.igvperu.gallery.navigation
 
-class NavigationComponentGallery
+import androidx.compose.runtime.Composable
+import com.pe.innari.igvperu.gallery.navigation.state.NavigationState
+import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
+
+class NavigationComponentGallery : ViewAmbient<NavigationState>() {
+
+    @Composable
+    override fun PortraitCompact() {
+
+    }
+}
