e01efca98ffd509be651b640a1e9c1ed5001a485
leandro1995 <leccbo1995@gmail.com>
2026-09-14 19:08:12 -0500

    :fire: EditTextComponent.kt: Eliminar importación no utilizada

--- app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/EditTextComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/edittext/EditTextComponent.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
-import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
