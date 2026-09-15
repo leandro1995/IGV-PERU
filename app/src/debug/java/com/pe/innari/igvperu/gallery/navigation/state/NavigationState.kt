d34a86e400361d763113d5fe0762d625e7e1342e
leandro1995 <leccbo1995@gmail.com>
2026-09-15 02:11:39 -0500

    :sparkles: NavigationState.kt: Implementar la propiedad backStack y la anotación Stable

--- app/src/debug/java/com/pe/innari/igvperu/gallery/navigation/state/NavigationState.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/navigation/state/NavigationState.kt
@@ -1,5 +1,10 @@
 package com.pe.innari.igvperu.gallery.navigation.state
 
-class NavigationState(
+import androidx.compose.runtime.Stable
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
 
+@Stable
+class NavigationState(
+    var backStack: NavBackStack<NavKey>
 )
