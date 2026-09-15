4d209b607d84d5626f5d5853d5dc69bfd392a174
leandro1995 <leccbo1995@gmail.com>
2026-09-15 02:39:22 -0500

    :sparkles: NavigationComponent.kt: Implementar callback para la configuración de vistas de navegación
    Descripción:
    * Agregar la propiedad privada `navigationCallBack` para gestionar la navegación
    * Actualizar el componente `NavDisplay` para invocar el callback dentro de `entryProvider`
    * Implementar el método `setNavigationView` para definir dinámicamente la lógica de navegación

--- app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/navigation/NavigationComponent.kt
@@ -1,24 +1,34 @@
 package com.pe.innari.igvperu.ui.component.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
+import com.pe.innari.igvperu.ui.component.navigation.callback.NavigationCallBack
 
 class NavigationComponent(private val backStack: NavBackStack<NavKey>) : ComponentAmbient() {
 
+    private var navigationCallBack: NavigationCallBack? = null
+
     @Composable
     override fun OnCreate() {
-        NavDisplay(
-            backStack = backStack,
-            onBack = {
-                backStack.removeLastOrNull()
-            },
-            entryProvider = entryProvider {
+        NavDisplay(backStack = backStack, onBack = {
+            backStack.removeLastOrNull()
+        }, entryProvider = entryProvider {
+            navigationCallBack?.navigate(entryProviderScope = this)
+        })
+    }
 
+    fun setNavigationView(method: (entryProviderScope: EntryProviderScope<NavKey>) -> Unit) {
+        if (navigationCallBack == null) {
+            navigationCallBack = object : NavigationCallBack {
+                override fun navigate(entryProviderScope: EntryProviderScope<NavKey>) {
+                    method(entryProviderScope)
+                }
             }
-        )
+        }
     }
 }
