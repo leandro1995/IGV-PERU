cddaa0434ebff3ef1101828e37b40d1735672a3d
leandro1995 <leccbo1995@gmail.com>
2026-09-14 19:01:58 -0500

    :sparkles: EditTextState.kt: Implementar la estructura del estado para el componente de edición de texto

--- app/src/debug/java/com/pe/innari/igvperu/gallery/editText/state/EditTextState.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/editText/state/EditTextState.kt
@@ -1,4 +1,10 @@
 package com.pe.innari.igvperu.gallery.editText.state
 
-class EditTextState {
-}
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.runtime.Stable
+
+@Stable
+class EditTextState(
+    var decimal: TextFieldState,
+    var integer: TextFieldState
+)
