a36cac2a9bd29acecf0337ddd282206fa4959a9d
leandro1995 <leccbo1995@gmail.com>
2026-09-15 02:11:03 -0500

    :sparkles: Navigation.kt: Definir los destinos de navegación Home y Detail
    Descripción:
    * Implementar la interfaz NavKey para definir las rutas de navegación
    * Agregar el objeto Home con la anotación Serializable
    * Agregar la clase de datos Detail con soporte para parámetros serializables

--- app/src/debug/java/com/pe/innari/igvperu/gallery/navigation/navigation/Navigation.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/navigation/navigation/Navigation.kt
@@ -1,3 +1,13 @@
 package com.pe.innari.igvperu.gallery.navigation.navigation
 
-sealed class Navigation
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+sealed class Navigation {
+
+    @Serializable
+    object Home : NavKey
+
+    @Serializable
+    data class Detail(val message: String) : NavKey
+}
