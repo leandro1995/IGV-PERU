6f67d5c1eada73c6f1dea3916d0fb66256cd245f
leandro1995 <leccbo1995@gmail.com>
2026-09-15 02:15:03 -0500

    :sparkles: NavigationComponentGallery.kt: Implementar la previsualización del componente de navegación
    Descripción:
    * Implementar el método InstanceMutableValues para inicializar el estado con el backstack
    * Configurar la vista PortraitCompact para instanciar el NavigationComponent
    * Agregar las importaciones necesarias para el manejo de navegación dentro de la galería

--- app/src/debug/java/com/pe/innari/igvperu/gallery/navigation/NavigationComponentGallery.kt
+++ app/src/debug/java/com/pe/innari/igvperu/gallery/navigation/NavigationComponentGallery.kt
@@ -1,13 +1,22 @@
 package com.pe.innari.igvperu.gallery.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.rememberNavBackStack
+import com.pe.innari.igvperu.extension.orEmpty
+import com.pe.innari.igvperu.gallery.navigation.navigation.Navigation
 import com.pe.innari.igvperu.gallery.navigation.state.NavigationState
+import com.pe.innari.igvperu.ui.component.navigation.NavigationComponent
 import com.pe.innari.igvperu.ui.view.ambient.ViewAmbient
 
 class NavigationComponentGallery : ViewAmbient<NavigationState>() {
 
     @Composable
-    override fun PortraitCompact() {
+    override fun InstanceMutableValues() {
+        mutableValues = NavigationState(backStack = rememberNavBackStack(Navigation.Home))
+    }
 
+    @Composable
+    override fun PortraitCompact() {
+        NavigationComponent(backStack = mutableValues?.backStack.orEmpty()).OnCreate()
     }
 }
