59806b3d78ae3c9fea912d60c287007dc19ed843
leandro1995 <leccbo1995@gmail.com>
2026-09-16 00:46:30 -0500

    :art: ButtonComponent.kt: Corregir nombre del método setOnClick

--- app/src/main/java/com/pe/innari/igvperu/ui/component/button/ButtonComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/button/ButtonComponent.kt
@@ -69,7 +69,7 @@ class ButtonComponent(private val buttonType: ButtonType, private val button: Bu
      *
      * @param method Acción a ejecutar cuando se presiona el botón.
      */
-    fun setOnclick(method: () -> Unit) {
+    fun setOnClick(method: () -> Unit) {
         if (buttonCallBack == null) {
             buttonCallBack = object : ButtonCallBack {
                 override fun onClick() {
