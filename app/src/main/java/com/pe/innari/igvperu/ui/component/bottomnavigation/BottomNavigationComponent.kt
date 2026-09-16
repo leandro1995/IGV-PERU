20c40cd3a2c22343dca1152851f1cb85a53bfde3
leandro1995 <leccbo1995@gmail.com>
2026-09-16 14:09:40 -0500

    :fire: BottomNavigationComponent.kt: Eliminar importación no utilizada de CompositionLocalProvider

--- app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
+++ app/src/main/java/com/pe/innari/igvperu/ui/component/bottomnavigation/BottomNavigationComponent.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.pe.innari.igvperu.ui.component.ambient.ComponentAmbient
