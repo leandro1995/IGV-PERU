e480c7a2fdab85f24b199905c9bb8b7a2a2d62af
leandro1995 <leccbo1995@gmail.com>
2026-09-16 21:03:20 -0500

    :sparkles: TimerBackground.kt: Implementar funcionalidad de temporizador en segundo plano
    Descripción:
    * Implementar la función Start para ejecutar acciones con retardo en Compose
    * Agregar lógica para convertir unidades de horas, minutos y segundos a milisegundos
    * Configurar el uso de LaunchedEffect para manejar la ejecución asíncrona del temporizador

--- app/src/main/java/com/pe/innari/igvperu/background/TimerBackground.kt
+++ app/src/main/java/com/pe/innari/igvperu/background/TimerBackground.kt
@@ -1,3 +1,33 @@
 package com.pe.innari.igvperu.background
 
-class TimerBackground
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.pe.innari.igvperu.background.type.TimeType
+import kotlinx.coroutines.delay
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.milliseconds
+
+class TimerBackground(private val time: Long = 0L, private val timeType: TimeType) {
+
+    @Composable
+    fun Start(method: () -> Unit) {
+        LaunchedEffect(Unit) {
+            delay(timeType())
+            method()
+        }
+    }
+
+    private fun timeType() = when (timeType) {
+        TimeType.HOURS -> {
+            TimeUnit.HOURS.toMillis(time).milliseconds
+        }
+
+        TimeType.MINUTES -> {
+            TimeUnit.MINUTES.toMillis(time).milliseconds
+        }
+
+        TimeType.SECONDS -> {
+            TimeUnit.SECONDS.toMillis(time).milliseconds
+        }
+    }
+}
