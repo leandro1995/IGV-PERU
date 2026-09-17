9322a837c44abca6ec3d04c20352616ca210d469
leandro1995 <leccbo1995@gmail.com>
2026-09-16 20:35:06 -0500

    :sparkles: MainActivity.kt: Implementar pantalla de inicio usando la API de SplashScreen

--- app/src/main/java/com/pe/innari/igvperu/MainActivity.kt
+++ app/src/main/java/com/pe/innari/igvperu/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.pe.innari.igvperu.ui.theme.IGVPERUTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val splashEnable = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
+        screenSplash.setKeepOnScreenCondition { splashEnable }
         enableEdgeToEdge()
         content()
     }
